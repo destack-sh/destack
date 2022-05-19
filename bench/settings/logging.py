67efb8abf8affcbcbb0072f25097d47c5097c6f3
Florian Cäsar <florian.caesar@pm.me>
2022-05-19 08:25:35 +0200

    chore: simplify logging

--- bench/settings/logging.py
+++ bench/settings/logging.py
@@ -7,62 +7,63 @@ import structlog
 from bench.settings import get_from_env
 from bench.settings.base import DEBUG, TEST
 
-LOGS_PATH: str = get_from_env("LOGS_PATH", "logs")
+LOG_LEVEL = os.getenv("DJANGO_LOG_LEVEL", "DEBUG" if TEST or DEBUG else "INFO")
+LOG_PATH: str = get_from_env("DJANGO_LOG_PATH", "logs")
+
+FORMATTERS = {
+    # "json_formatter": {
+    #     "()": structlog.stdlib.ProcessorFormatter,
+    #     "processor": structlog.processors.JSONRenderer(),
+    # },
+    "plain_console": {
+        "()": structlog.stdlib.ProcessorFormatter,
+        "processor": structlog.dev.ConsoleRenderer(pad_event=0),
+    },
+    "key_value": {
+        "()": structlog.stdlib.ProcessorFormatter,
+        "processor": structlog.processors.KeyValueRenderer(
+            key_order=["timestamp", "level", "event", "logger"]
+        ),
+    },
+}
+
 HANDLERS = {
     "console": {
         "class": "logging.StreamHandler",
         "formatter": "plain_console",
     },
+    "flat_line_file": {"class": "logging.NullHandler"},
+    "null": {
+        "class": "logging.NullHandler",
+    },
 }
 
 if DEBUG and not TEST:
     # log to file and ensure corresponding directory exists
-    logged_handlers = ["console", "flat_line_file"]
-    Path(LOGS_PATH).mkdir(exist_ok=True)
+    Path(LOG_PATH).mkdir(exist_ok=True)
     # configure file handlers only if needed as all handlers are instantiated
     #  and file handlers fail is their path does not exist
-    HANDLERS["flat_line_file"] = {
-        "class": "logging.handlers.RotatingFileHandler",
-        "filename": os.path.join(LOGS_PATH, "flat_line.log"),
-        "formatter": "key_value",
-    }
-
+    # HANDLERS["flat_line_file"] = {
+    #     "class": "logging.handlers.RotatingFileHandler",
+    #     "filename": os.path.join(LOG_PATH, "flat_line.log"),
+    #     "formatter": "json_formatter",
+    # }
+    logged_handlers = ["console"]
 else:
     logged_handlers = ["console"]
 
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": True,
-    "formatters": {
-        "json_formatter": {
-            "()": structlog.stdlib.ProcessorFormatter,
-            "processor": structlog.processors.JSONRenderer(),
-        },
-        "plain_console": {
-            "()": structlog.stdlib.ProcessorFormatter,
-            "processor": structlog.dev.ConsoleRenderer(pad_event=0),
-        },
-        "key_value": {
-            "()": structlog.stdlib.ProcessorFormatter,
-            "processor": structlog.processors.KeyValueRenderer(
-                key_order=["timestamp", "level", "event", "logger"]
-            ),
-        },
-    },
+    "formatters": FORMATTERS,
     "handlers": HANDLERS,
     "loggers": {
-        "django_structlog": {
-            "handlers": logged_handlers,
-            "level": "INFO",
-        },
-        "bench": {
-            "handlers": logged_handlers,
-            "level": "INFO",
-        },
+        "django_structlog": {"handlers": logged_handlers, "level": LOG_LEVEL, "propagate": False},
+        "axes": {"handlers": logged_handlers, "level": LOG_LEVEL, "propagate": False},
+        "bench": {"handlers": logged_handlers, "level": LOG_LEVEL, "propagate": False},
     },
 }
 
-# use structlog logging only
 structlog.configure(
     processors=[
         structlog.stdlib.filter_by_level,
