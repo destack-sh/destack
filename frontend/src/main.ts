883eb674874b622f8fc9258d6173dd1963edfc25
Florian Cäsar <florian.caesar@pm.me>
2022-06-22 13:10:08 +0200

    chore(frontend): resolve json imports

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from "vue";
+/* eslint-disable no-console */
+
 import { createPinia } from "pinia";
+import { createApp } from "vue";
+import { version } from "../../package.json";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,3 +13,16 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+console.group(`%c✨ Project Information`, "color:orangered"); // groupCollapsed
+
+if (import.meta.env.DEV) {
+  console.info(`%cVersion: v${version}`, "color:orangered");
+}
+
+console.info(`%cEnvironment: ${import.meta.env.MODE}`, "color:orangered");
+console.groupEnd();
+
+// prevent opening files that are dragged over the window
+window.addEventListener("dragover", (e) => e.preventDefault(), false);
+window.addEventListener("drop", (e) => e.preventDefault(), false);
