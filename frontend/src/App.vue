777de2b702337b2dc609e921863308224a30193e
Florian Cäsar <florian.caesar@pm.me>
2022-06-24 14:05:56 +0200

    refactor: reorganize frontend components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,8 @@
+<template>
+  <div v-if="appStore.hydrating" class="pt-8 text-center">Loading...</div>
+  <RouterView v-else />
+</template>
+
 <script setup lang="ts">
 import { useMeta } from "vue-meta";
 import { RouterView } from "vue-router";
@@ -7,11 +12,6 @@ useMeta({ title: "Bench" });
 
 const appStore = useAppStore();
 </script>
-
-<template>
-  <div v-if="appStore.hydrating">loading</div>
-  <RouterView v-else />
-</template>
 
 <style>
 @import "@/assets/base.css";
