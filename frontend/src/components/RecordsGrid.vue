3e6c771999887259556128382cf32be5c19f1d29
Florian Cäsar <florian.caesar@pm.me>
2022-07-08 18:32:18 +0200

    feat: create new model via ui

---
+++ frontend/src/components/RecordsGrid.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mt-8 flex flex-col">
+    <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+      <div class="inline-block min-w-full py-2 align-middle">
+        <div class="shadow-sm ring-1 ring-black ring-opacity-5">
+          <table class="min-w-full border-separate" style="border-spacing: 0">
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  v-for="column in columns"
+                  :key="column.name"
+                  class="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                >
+                  Name
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white">
+              <tr v-for="(record, recordIdx) in records" :key="record.email">
+                <td
+                  v-for="column in columns"
+                  :key="column.name"
+                  :class="[
+                    recordIdx !== records.length - 1 ? 'border-b border-gray-200' : '',
+                    'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8',
+                  ]"
+                >
+                  {{ record[column.name] }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const columns = [];
+const records = [];
+</script>
---
+++ frontend/src/interfaces/TextInterface.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    type="text"
+    :value="text"
+    @input="(event) => $emit('update:text', event.target)"
+    name="email"
+    class="block w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 sm:text-sm"
+    :placeholder="placeholder"
+  />
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+  text: string;
+  placeholder?: string;
+}>();
+
+defineEmits(["update:text"]);
+</script>
