603b177376fd91061c090c5c9346e44a890844c0
Florian Cäsar <florian.caesar@pm.me>
2022-07-10 20:53:51 +0200

    feat: sync model handlers to frontend

--- frontend/src/types/spec.ts
+++ frontend/src/types/spec.ts
@@ -1,5 +1,49 @@
-export type FieldSpec = {
+type _Type = {
+  _type: string;
+};
+
+type _Spec = {
   name: string;
   description?: string;
-  type: "string";
+};
+
+export type ValueType = _Type & {
+  _type: "ValueType";
+  dtype: string;
+};
+
+export type FieldType = {
+  _type: string;
+};
+
+export type FieldSpec = _Spec & {
+  type: FieldType | Array<FieldSpec> | Record<string, FieldSpec>;
+};
+
+export type RecordSpec = FieldSpec;
+
+export type ArtifactSpec = _Spec & {
+  type: string;
+};
+
+export type DatasetSpec = ArtifactSpec & {
+  type: "dataset";
+  record_spec: RecordSpec;
+  config_spec: ConfigSpec;
+};
+
+export type ModelSpec = ArtifactSpec & {
+  type: "model";
+  input_spec: RecordSpec;
+  output_spec: RecordSpec;
+  config_spec: ConfigSpec;
+};
+
+export type ConfigSpec = _Spec & {
+  type: Record<string, ArtifactSpec | FieldSpec>;
+};
+
+export type ModelHandlerSpec = _Spec & {
+  base_spec: ModelSpec;
+  config_spec: ConfigSpec;
 };
