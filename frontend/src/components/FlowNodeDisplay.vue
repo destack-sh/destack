f2fc221db650653f5bedabff57e3bca283baab80
Florian Cäsar <florian.caesar@pm.me>
2022-08-09 18:45:07 +0200

    feat: set dynamic connection ports for flow nodes

--- frontend/src/components/FlowNodeDisplay.vue
+++ frontend/src/components/FlowNodeDisplay.vue
@@ -2,12 +2,12 @@
   <div class="h-full w-full divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
     <div class="flex flex-row items-center justify-between px-4 py-2 sm:px-6">
       <div>
-        <h3 class="text-md font-medium leading-6 text-gray-900">{{ node.name }}</h3>
+        <h3 class="text-md font-medium leading-6 text-gray-900">{{ name }}</h3>
         <h5 class="text-xs font-normal text-gray-500">{{ label }}</h5>
       </div>
       <div>
         <!-- TODO @UI @Bug: Menu in FlowNodeInterface is cropped to local container -->
-        <Menu as="div" class="relative inline-block text-left">
+        <Menu as="div" class="inline-block text-left">
           <div>
             <MenuButton
               class="flex items-center rounded-full text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 focus:ring-offset-gray-100"
@@ -26,7 +26,7 @@
             leave-to-class="transform opacity-0 scale-95"
           >
             <MenuItems
-              class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              class="absolute right-0 z-20 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
             >
               <MenuItem v-slot="{ active }" v-for="action in actions" :key="action.key">
                 <a
@@ -56,8 +56,6 @@
   </div>
 </template>
 <script lang="ts" setup>
-import type { FlowNode } from "@/types";
-
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 import { DotsVerticalIcon, DuplicateIcon, PencilAltIcon, TrashIcon } from "@heroicons/vue/solid";
 
@@ -85,6 +83,6 @@ const actions = [
 
 const props = defineProps<{
   label: string;
-  node: FlowNode;
+  name: string;
 }>();
 </script>
