575cd153fe8ea9afeef5ce3f2c4bb7660534154e
Florian Cäsar <florian.caesar@pm.me>
2022-09-14 22:22:25 +0200

    bench: set dataset record index in dataset accessor read

--- bench/api/dataset.py
+++ bench/api/dataset.py
@@ -71,6 +71,7 @@ class DatasetVersionSerializer(ArtifactVersionSerializer):
 
 class DatasetRecordSerializer(serializers.Serializer):
     id = serializers.UUIDField(required=False)
+    index = serializers.IntegerField(required=False)
     data = serializers.JSONField()
     metadata = serializers.JSONField(required=False, default=None)
 
@@ -78,8 +79,8 @@ class DatasetRecordSerializer(serializers.Serializer):
         return DatasetRecord(**validated_data)
 
     class Meta:
-        fields = ["id", "data", "metadata"]
-        read_only_fields = ["id"]
+        fields = ["id", "data", "metadata", "index"]
+        read_only_fields = ["id", "index"]
 
 
 class DatasetViewSet(ArtifactViewSet):
