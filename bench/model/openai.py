dff4de648bb87e300e76c58508e0241b8679f65f
Florian Cäsar <florian.caesar@pm.me>
2022-10-03 21:51:13 +0200

    perf: add async batched model handler, add openai async batch

--- bench/model/openai.py
+++ bench/model/openai.py
@@ -1,16 +1,16 @@
 import abc
 import enum
-from typing import List, Union, cast
+from typing import Union, cast
 
-import openai
+import aiohttp
 
-from bench.model.base import ModelHandlerMetadata, UnbatchedModelHandler, models
+from bench.model.base import AsyncBatchedModelHandler, ModelHandlerMetadata, models
 from bench.model.utils import make_scored_labels_spec
 from bench.utils.record import Record, RecordBatch, RecordList
 from bench.utils.spec import ModelType, convert_to_record_spec
 
 
-class OpenAIModel(UnbatchedModelHandler, abc.ABC):
+class OpenAIModel(AsyncBatchedModelHandler, abc.ABC):
     config_static_keys: set[str] = set()  # entire config can be changed dynamically
 
     class Engine(enum.Enum):
@@ -24,32 +24,35 @@ class OpenAIModel(UnbatchedModelHandler, abc.ABC):
     def __init__(
         self,
         api_key: str,
-        engine_id: Engine = Engine.Ada,
+        model: Engine = Engine.Ada,
         max_tokens: int = 128,
         temperature: float = 0.7,
         top_p: float = 1.0,
         n: int = 1,
-        stop: List[str] = None,
-        **kwargs
+        stop: list[str] = None,
+        **kwargs,
     ):
         super().__init__(**kwargs)
         self._api_key = api_key
-        self.engine_id = engine_id.value
+        self.model = model.value
         self.max_tokens = max_tokens
         self.temperature = temperature
         self.top_p = top_p
         self.n = n
         self.stop = stop
 
+    @property
+    def headers(self):
+        return {"Content-Type": "application/json", "Authorization": f"Bearer {self._api_key}"}
+
     def _get_params(self):
         return dict(
-            engine=self.engine_id,
+            model=self.model,
             max_tokens=self.max_tokens,
             temperature=self.temperature,
             top_p=self.top_p,
             n=self.n,
             stop=self.stop,
-            api_key=self._api_key,
         )
 
 
@@ -66,9 +69,16 @@ class OpenAIModelForCompletion(OpenAIModel):
     )
     base_spec = spec
 
-    def run(self, record: Record) -> Union[Record, RecordBatch]:
+    async def run_async(self, record: Record) -> Union[Record, RecordBatch]:
         record = cast(dict, record)
-        output = openai.Completion.create(prompt=record["text"], **self._get_params())
+        request = {"model": self.model, "prompt": record["text"], **self._get_params()}
+
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.post(
+                "https://api.openai.com/v1/completions", data=request
+            ) as response:
+                output = await response.json()
+
         output_records = [choice for choice in output["choices"]]
         if self.n == 1:
             return output_records[0]
@@ -95,14 +105,22 @@ class OpenAIModelForClassification(OpenAIModel):
     )
     base_spec = spec
 
-    def run(self, record: Record) -> Union[Record, RecordBatch]:
+    async def run_async(self, record: Record) -> Union[Record, RecordBatch]:
         record = cast(dict, record)
-        output = openai.Classification.create(
-            prompt=record["text"],
-            examples=record["examples"],
-            labels=record["labels"],
-            **self._get_params()
-        )
+        request = {
+            "model": self.model,
+            "prompt": record["text"],
+            "examples": record["examples"],
+            "labels": record["labels"],
+            **self._get_params(),
+        }
+
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.post(
+                "https://api.openai.com/v1/classifications", data=request
+            ) as response:
+                output = await response.json()
+
         output_records = [choice for choice in output["choices"]]
         if self.n == 1:
             return output_records[0]
