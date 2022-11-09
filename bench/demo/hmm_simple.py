f3542e02bbe0f8dc0c4c6922de57fe069ab5fcd8
Florian Cäsar <florian.caesar@pm.me>
2022-11-09 16:11:29 +0100

    refactor: simplify API

--- bench/demo/hmm_simple.py
+++ bench/demo/hmm_simple.py
@@ -2,10 +2,9 @@
 from typing import Any
 
 from bench.model.base import ModelHandler
-from bench.models import DatasetVersion
+from bench.models import Dataset
 
 Model = ModelHandler
-Dataset = DatasetVersion
 benv: Any = {}
 # @/bench
 
