e02e9456e6d226e7d448494dcef4a2128b633437
Florian Cäsar <florian.caesar@pm.me>
2022-11-14 10:24:54 +0100

    fix: use preset properly for grapqhl codegen

--- codegen.ts
+++ codegen.ts
@@ -4,6 +4,7 @@ const config: CodegenConfig = {
   schema: "./schema.gen.graphql",
   documents: ["frontend/src/**/*.vue", "frontend/src/**/*.ts"],
   ignoreNoDocuments: true,
+  hooks: { afterOneFileWrite: ["prettier --write"] },
   generates: {
     "./frontend/src/gql/": {
       preset: "client",
@@ -11,8 +12,24 @@ const config: CodegenConfig = {
         useTypeImports: true,
         withCompositionFunctions: true,
       },
-      plugins: ["typescript", "typescript-operations", "typescript-vue-apollo"],
+      plugins: ["typescript-vue-apollo"],
     },
+    // below: alternative config if we want to put operations near their definitions
+    // "frontend/src/gql/types.ts": {
+    //   config: {
+    //     useTypeImports: true,
+    //     withCompositionFunctions: true,
+    //   },
+    //   plugins: ["typescript"],
+    // },
+    // "frontend/src/": {
+    //   preset: "near-operation-file",
+    //   presetConfig: {
+    //     extension: ".gql.ts",
+    //     baseTypesPath: "gql/types.ts",
+    //   },
+    //   plugins: ["typescript-operations", "typescript-vue-apollo"],
+    // },
   },
 };
 
--- frontend/src/gql/graphql.ts
+++ frontend/src/gql/graphql.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -21,62 +21,122 @@ export type Scalars = {
 /** An object with a Globally Unique ID */
 export type Node = {
   /** The Globally Unique ID of this object */
-  id: Scalars['GlobalID'];
+  id: Scalars["GlobalID"];
 };
 
 export type Organization = Node & {
-  __typename?: 'Organization';
-  createdAt: Scalars['DateTime'];
-  id: Scalars['GlobalID'];
-  name: Scalars['String'];
-  slug: Scalars['String'];
-  updatedAt: Scalars['DateTime'];
+  __typename?: "Organization";
+  createdAt: Scalars["DateTime"];
+  id: Scalars["GlobalID"];
+  members: Array<User>;
+  name: Scalars["String"];
+  projects: Array<Project>;
+  slug: Scalars["String"];
+  updatedAt: Scalars["DateTime"];
+};
+
+/** A connection to a list of items. */
+export type OrganizationConnection = {
+  __typename?: "OrganizationConnection";
+  /** Contains the nodes in this connection */
+  edges: Array<OrganizationEdge>;
+  /** Pagination data for this connection */
+  pageInfo: PageInfo;
+  /** Total quantity of existing nodes */
+  totalCount?: Maybe<Scalars["Int"]>;
+};
+
+/** An edge in a connection. */
+export type OrganizationEdge = {
+  __typename?: "OrganizationEdge";
+  /** A cursor for use in pagination */
+  cursor: Scalars["String"];
+  /** The item at the end of the edge */
+  node: Organization;
+};
+
+/** Information to aid in pagination. */
+export type PageInfo = {
+  __typename?: "PageInfo";
+  /** When paginating forwards, the cursor to continue. */
+  endCursor?: Maybe<Scalars["String"]>;
+  /** When paginating forwards, are there more items? */
+  hasNextPage: Scalars["Boolean"];
+  /** When paginating backwards, are there more items? */
+  hasPreviousPage: Scalars["Boolean"];
+  /** When paginating backwards, the cursor to continue. */
+  startCursor?: Maybe<Scalars["String"]>;
+};
+
+export type Project = Node & {
+  __typename?: "Project";
+  createdAt: Scalars["DateTime"];
+  id: Scalars["GlobalID"];
+  name: Scalars["String"];
+  organization: Organization;
+  slug: Scalars["String"];
+  updatedAt: Scalars["DateTime"];
+};
+
+/** A connection to a list of items. */
+export type ProjectConnection = {
+  __typename?: "ProjectConnection";
+  /** Contains the nodes in this connection */
+  edges: Array<ProjectEdge>;
+  /** Pagination data for this connection */
+  pageInfo: PageInfo;
+  /** Total quantity of existing nodes */
+  totalCount?: Maybe<Scalars["Int"]>;
+};
+
+/** An edge in a connection. */
+export type ProjectEdge = {
+  __typename?: "ProjectEdge";
+  /** A cursor for use in pagination */
+  cursor: Scalars["String"];
+  /** The item at the end of the edge */
+  node: Project;
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   organization?: Maybe<Organization>;
+  organizationsConnection: OrganizationConnection;
+  project?: Maybe<Project>;
+  projectsConnection: ProjectConnection;
 };
-
 
 export type QueryOrganizationArgs = {
-  id: Scalars['GlobalID'];
+  id: Scalars["GlobalID"];
 };
 
-/** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-  /** Date with time (isoformat) */
-  DateTime: any;
-  /** The `ID` scalar type represents a unique identifier, often used to refetch an object or as key for a cache. The ID type appears in a JSON response as a String; however, it is not intended to be human-readable. When expected as an input type, any string (such as `"4"`) or integer (such as `4`) input value will be accepted as an ID. */
-  GlobalID: any;
+export type QueryOrganizationsConnectionArgs = {
+  after?: InputMaybe<Scalars["String"]>;
+  before?: InputMaybe<Scalars["String"]>;
+  first?: InputMaybe<Scalars["Int"]>;
+  last?: InputMaybe<Scalars["Int"]>;
 };
 
-/** An object with a Globally Unique ID */
-export type Node = {
-  /** The Globally Unique ID of this object */
-  id: Scalars['GlobalID'];
+export type QueryProjectArgs = {
+  id: Scalars["GlobalID"];
 };
 
-export type Organization = Node & {
-  __typename?: 'Organization';
-  createdAt: Scalars['DateTime'];
-  id: Scalars['GlobalID'];
-  name: Scalars['String'];
-  slug: Scalars['String'];
-  updatedAt: Scalars['DateTime'];
+export type QueryProjectsConnectionArgs = {
+  after?: InputMaybe<Scalars["String"]>;
+  before?: InputMaybe<Scalars["String"]>;
+  first?: InputMaybe<Scalars["Int"]>;
+  last?: InputMaybe<Scalars["Int"]>;
 };
 
-export type Query = {
-  __typename?: 'Query';
-  organization?: Maybe<Organization>;
-};
-
-
-export type QueryOrganizationArgs = {
-  id: Scalars['GlobalID'];
+export type User = Node & {
+  __typename?: "User";
+  createdAt: Scalars["DateTime"];
+  email: Scalars["String"];
+  firstName: Scalars["String"];
+  id: Scalars["GlobalID"];
+  lastName: Scalars["String"];
+  organizations: Array<Organization>;
+  updatedAt: Scalars["DateTime"];
+  /** Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only. */
+  username: Scalars["String"];
 };
