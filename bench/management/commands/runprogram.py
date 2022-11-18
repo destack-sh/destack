10838fb3dbb4bf55614914b5c89dbaf3a5db8571
Florian Cäsar <florian.caesar@pm.me>
2022-11-18 14:56:06 +0100

    feat: update project main program, add skeleton run

---
+++ bench/management/commands/runprogram.py
@@ -0,0 +1,35 @@
+from asgiref.sync import async_to_sync
+from django.core.management import BaseCommand, CommandParser
+
+from bench.executor import Executor
+from bench.models import Organization, Project
+from bench.models.project import ProjectType
+
+
+class Command(BaseCommand):
+    help = "Runs a project's program with the given arguments"
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        # project as organization/project
+        parser.add_argument("project", type=str)
+        # arbitrary kwargs
+        parser.add_argument("kwargs", type=str, nargs="*")
+
+    def handle(self, project: str, *args, **options):
+        organization, project = project.split("/")
+        organization = Organization.objects.get(slug=organization)
+        project = Project.objects.get(slug=project, organization=organization)
+        if project.type != ProjectType.EXECUTABLE:
+            raise ValueError(f"project must be executable: {project}")
+        project_version = project.head
+        program = project_version.program
+        if program is None:
+            raise ValueError(f"project version has no program: {project_version}")
+
+        # TODO @Feature: set current compiled project builds automatically
+        #  (and allow for multiple builds?, ask if there is more than one)
+        # just use the latest version of the program for now
+        compiled_program = program.implementations.order_by("-created_at").first()
+
+        executor = Executor()
+        async_to_sync(executor.run)(compiled_program, options["kwargs"])
