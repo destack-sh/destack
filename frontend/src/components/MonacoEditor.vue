86c1a2856fc9f54da28ebf17d132c47ac640b160
Florian Cäsar <florian.caesar@pm.me>
2022-11-21 11:05:32 +0100

    chore: harden strawberry for prod

---
+++ frontend/src/components/MonacoEditor.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="container" class="h-40 w-1/2"></div>
+</template>
+<script lang="ts" setup>
+import * as monaco from "monaco-editor";
+
+monaco.editor.create(document.getElementById("container"), {
+  value: "def test()",
+  language: "python",
+});
+</script>
