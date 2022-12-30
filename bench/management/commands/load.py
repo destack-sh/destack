dbf9c050ce2e785f0670107252fa757b472ec015
Florian Cäsar <florian.caesar@pm.me>
2022-12-30 16:38:48 +0100

    fix: group mapped statement contents correctly

--- bench/management/commands/load.py
+++ bench/management/commands/load.py
@@ -8,7 +8,7 @@ from django.core.management import BaseCommand
 from django.core.management.base import CommandParser
 from django.db import transaction
 
-from bench.backend.mapper import write
+from bench.backend.mapper import lookup_module_in_db, rmap_module, write
 from bench.language import lex, parse
 from bench.language.lex import SourceFile
 from bench.models import Organization, Project
@@ -40,8 +40,9 @@ class Command(BaseCommand):
 
         project_v = project.create_version(name=version_id)
         project_v.reset()
+        module = rmap_module(project_v)
         source_file = SourceFile(path=path, content=Path(path).read_text())
-        language_files = parse(lex(source_file)).files
+        language_files = parse(lex(source_file), module, lookup_module_in_db).files
         write(language_files, project_v)
 
         # advance head to new version
