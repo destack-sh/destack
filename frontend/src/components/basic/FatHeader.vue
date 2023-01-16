23a26206272b18b481add055e4c7c5287b7b1d72
Florian Cäsar <florian.caesar@pm.me>
2023-01-16 22:01:21 +0100

    refactor: factor out some bench ui elements

---
+++ frontend/src/components/basic/FatHeader.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+<template>
+  <!-- Standard fat header -->
+  <header
+    class="static mx-auto flex w-full flex-shrink-0 justify-between gap-8 overflow-y-visible border-b border-gray-200 bg-white shadow-sm"
+  >
+    <!-- Left side -->
+    <div class="static flex items-center">
+      <slot name="left" />
+    </div>
+    <div class="flex min-w-fit flex-shrink-0 items-center justify-end">
+      <slot name="right" />
+    </div>
+  </header>
+</template>
