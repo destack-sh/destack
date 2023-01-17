f3ac0a7428eee4dd4cb0650d3a069fc488c26785
Florian Cäsar <florian.caesar@pm.me>
2023-01-17 11:09:58 +0100

    chore: add basic compile and run error types

--- bench/backend/compile.py
+++ bench/backend/compile.py
@@ -1,10 +1,37 @@
 from __future__ import annotations
 
 import enum
+from typing import Optional
 
 import structlog
 
+from bench.language import Statement
+
 logger = structlog.get_logger(__name__)
+
+
+class CompileErrorType(enum.Enum):
+    INTERNAL = 0, "Internal error"
+    RUN = 1, "Error running user code"
+
+    def __new__(cls, value, description):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.description = description
+        return obj
+
+
+class CompileError(ValueError):
+    def __init__(
+        self,
+        _t: CompileErrorType,
+        statement: Optional[Statement],
+        cause: Optional[Exception] = None,
+    ):
+        self.type = _t
+        self.statement = statement
+        self.cause = cause
+        super().__init__(self.type.description)
 
 
 class ExpectationStatementType(enum.Enum):
