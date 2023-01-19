a0400eefd9131011ae86d00937e938dddb1785f3
Florian Cäsar <florian.caesar@pm.me>
2023-01-19 08:40:11 +0100

    chore: hook up module runtime to subscription

--- bench/api/realtime.py
+++ bench/api/realtime.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from typing import AsyncGenerator, Optional
 from uuid import UUID
 
@@ -5,7 +6,8 @@ import zmq
 from strawberry_django_plus import gql
 from strawberry_django_plus.relay import GlobalID
 
-from bench.api.symbol import Statement, TypeNode
+from bench.api.symbol import Statement, StatementType, SymbolType, TypeNode
+from bench.language import wire
 from bench.runtime.worker import ReqModuleRuntimePayload
 from bench.settings import ZMQ_RUNTIME_WORKER_ADDR
 from bench.zmq import ZMessage, ZMessageType, recv_message_with, send_message, zmq_ctx
@@ -13,29 +15,74 @@ from bench.zmq.messages import RepModuleRuntimePayload
 
 
 @gql.type
-class Job:
+class InterpModule:
     id: UUID
     name: str
+    files: list["InterpFile"]
 
 
 @gql.type
-class InterpStatement(Statement):
-    type: Optional[TypeNode]
+class InterpFile:
+    id: UUID
+    module: InterpModule
+    path: str
+    statements: list["InterpStatement"]
+
+
+@gql.type
+class InterpStatement:
+    id: UUID
+    file: InterpFile
+    name: Optional[str]
+    type: StatementType
+    symbol_type: Optional[SymbolType]
+    type_node: Optional[TypeNode]
 
 
 @gql.type
 class ModuleRuntime:
-    id: UUID
-    jobs: list[Job]
-    symbols: list[InterpStatement]
-    errors: list["ModuleError"]
+    module: InterpModule
+    errors: list["Error"]
 
 
 @gql.type
-class ModuleError:
+class Error:
     type: str
     message: str
     statement: Statement
+
+
+def rmap_module(wire_module: wire.ModuleData) -> InterpModule:
+    """Maps a wire module into a GQL interpreted module"""
+    interp_module = InterpModule(id=wire_module.id, name=wire_module.name, files=[])
+    for file in wire_module.files:
+        interp_file = InterpFile(id=file.id, module=interp_module, path=file.path, statements=[])
+        interp_module.files.append(interp_file)
+        for statement in file.statements:
+            interp_statement = InterpStatement(
+                id=statement.id,
+                file=interp_file,
+                name=statement.name,
+                type=statement.type,
+                symbol_type=statement.symbol_type,
+                type_node=statement.type_node,
+            )
+            interp_file.statements.append(interp_statement)
+    return interp_module
+
+
+def rmap_errors(wire_errors: list[wire.ErrorData], module: InterpModule) -> list[Error]:
+    """Maps a wire error into a GQL error"""
+    statements_by_id = {}
+    for statement in chain.from_iterable(file.statements for file in module.files):
+        statements_by_id[statement.id] = statement
+
+    errors = []
+    for error in wire_errors:
+        statement = statements_by_id[error.statement_id] if error.statement_id else None
+        error = Error(type=error.type, message=error.message, statement=statement)
+        errors.append(error)
+    return errors
 
 
 @gql.type
@@ -50,17 +97,22 @@ class ModuleRuntimeSubscription:
         worker_sub_sock = zmq_ctx.socket(zmq.SUB)
         worker_sub_sock.connect(ZMQ_RUNTIME_WORKER_ADDR)
 
+        # get initial runtime
         req_module_runtime_msg = ZMessage(
             ZMessageType.REQ_MODULE_RUNTIME, ReqModuleRuntimePayload(module_id=project_version_id)
         )
         send_message(worker_req_sock, req_module_runtime_msg)
-        reply, module_runtime = await recv_message_with(worker_req_sock, RepModuleRuntimePayload)
-        yield ModuleRuntime(module_id=module_runtime.module_id, tasks=[], symbols=[], errors=[])
+        _, payload = await recv_message_with(worker_req_sock, RepModuleRuntimePayload)
+        module = rmap_module(payload.module)
+        yield ModuleRuntime(module=module, errors=rmap_errors(payload.errors, module))
 
-        # TODO @Incomplete: get module state updates from the worker via zmq server/client then pub/sub
+        # get runtime changes
         try:
-            for module_runtime in []:
-                yield module_runtime
+            while True:
+                _, update = await recv_message_with(worker_sub_sock, RepModuleRuntimePayload)
+                module = rmap_module(update.module)
+                yield ModuleRuntime(module=module, errors=rmap_errors(update.errors, module))
+
         finally:
             worker_req_sock.close()
             worker_sub_sock.close()
