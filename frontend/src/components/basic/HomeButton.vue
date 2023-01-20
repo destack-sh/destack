058020717291dae19889629aed37e8f871dcc6d8
Florian Cäsar <florian.caesar@pm.me>
2023-01-20 17:49:26 +0100

    chore: make bench logo slightly less shitty

--- frontend/src/components/basic/HomeButton.vue
+++ frontend/src/components/basic/HomeButton.vue
@@ -2,10 +2,11 @@
 <template>
   <div class="flex flex-shrink-0 items-center px-4 py-2 hover:bg-gray-50">
     <a href="#">
-      <svg viewBox="0 0 100 100" class="h-8 w-8 text-orange-500">
-        <!-- A narrow X  -->
-        <path d="M 10 10 L 90 90 M 90 10 L 10 90" stroke="currentColor" stroke-width="25" fill="none" />
-      </svg>
+      <div class="text-lg font-extrabold text-orange-600">
+        <span class="text-yellow-900">[</span>
+        <span class="text-xl">x</span>
+        <span class="text-yellow-900">]</span>
+      </div>
     </a>
   </div>
 </template>
