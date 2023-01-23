2c7c482bc9d93b1a16694fc054aaf85eb6d5ae92
Florian Cäsar <florian.caesar@pm.me>
2023-01-23 10:21:43 +0100

    chore: set icon border to green

--- frontend/src/components/basic/HomeButton.vue
+++ frontend/src/components/basic/HomeButton.vue
@@ -3,9 +3,9 @@
   <div class="flex flex-shrink-0 items-center px-4 py-2 hover:bg-gray-50">
     <a href="#">
       <div class="text-lg font-extrabold text-orange-600">
-        <span class="text-yellow-900">[</span>
+        <span class="text-emerald-900">[</span>
         <span class="text-xl">x</span>
-        <span class="text-yellow-900">]</span>
+        <span class="text-emerald-900">]</span>
       </div>
     </a>
   </div>
