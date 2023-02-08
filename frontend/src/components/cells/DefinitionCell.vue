78bd4f66fb6df179e6d72c839bffa99d381d4cfe
Florian Cäsar <florian.caesar@pm.me>
2023-02-08 08:37:37 +0100

    refactor: rename BlankCell -> SelectTypeCell

--- frontend/src/components/cells/DefinitionCell.vue
+++ frontend/src/components/cells/DefinitionCell.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import BlankCell from "@/components/cells/BlankCell.vue";
+import SelectTypeCell from "@/components/cells/SelectTypeCell.vue";
 import ModifierCell from "@/components/cells/ModifierCell.vue";
 import SymbolTypeCell from "@/components/cells/SymbolTypeCell.vue";
 import EditableSpan from "@/components/EditableSpan.vue";
@@ -10,7 +10,7 @@ const context = useStatementContext();
 
 const name: Ref<string> = ref(context.statement.value.name ?? "");
 context.syncName(name);
-const gapRef: Ref<InstanceType<typeof BlankCell> | null> = ref(null);
+const gapRef: Ref<InstanceType<typeof SelectTypeCell> | null> = ref(null);
 const nameRef: Ref<InstanceType<typeof EditableSpan> | null> = ref(null);
 
 function deleteModifierOrSelf() {
@@ -32,7 +32,7 @@ defineExpose({
 <template>
   <div class="flex flex-row flex-wrap gap-1">
     <ModifierCell v-if="context.statement.value.modifier" />
-    <BlankCell
+    <SelectTypeCell
       class="-mx-0.5"
       ref="gapRef"
       @navigate-up="context.navigateUp"
