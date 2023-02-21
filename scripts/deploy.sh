a6b376a843022fa931c44465899124c9f30330ba
Florian Cäsar <florian.caesar@pm.me>
2023-02-21 19:44:13 +0100

    chore: add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# check that the repo is clean
+if [[ -n $(git status --porcelain) && "$1" != "--force" ]]; then
+    echo "Repo is not clean. Aborting."
+    exit 1
+fi
+
+# call build_push.sh
+./scripts/build_push.sh
+
+# deploy with pulumi
+pulumi --cwd infra up -y
