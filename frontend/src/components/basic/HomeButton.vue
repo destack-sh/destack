451536dac8ab6c95d0c0ad3f1079958f804110ea
Florian Cäsar <florian.caesar@pm.me>
2023-02-23 10:45:36 +0100

    chore: scaffold complete signup page

--- frontend/src/components/basic/HomeButton.vue
+++ frontend/src/components/basic/HomeButton.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup></script>
 <template>
-  <div class="flex flex-shrink-0 items-center px-[13px] py-2 hover:bg-gray-50">
-    <RouterLink to="/">
+  <div class="group flex flex-shrink-0 items-center">
+    <RouterLink to="/" class="px-[13px] py-2 hover:bg-orange-50">
       <div class="font-mono text-lg font-bold">
-        <span class="-mx-0.5 text-gray-900">[</span>
+        <span class="-mx-0.5 inline-block translate-x-0 text-gray-900 transition-transform group-hover:-translate-x-0.5"
+          >[</span
+        >
         <span class="text-xl text-orange-600">x</span>
-        <span class="-mx-0.5 text-gray-900">]</span>
+        <span class="-mx-0.5 inline-block translate-x-0 text-gray-900 transition-transform group-hover:translate-x-0.5"
+          >]</span
+        >
       </div>
     </RouterLink>
   </div>
---
+++ frontend/src/routes/CompleteSignup.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import FatHeader from "@/components/basic/FatHeader.vue";
+import HomeButton from "@/components/basic/HomeButton.vue";
+import { useTitle } from "@vueuse/core";
+
+const props = defineProps<{ next?: string }>();
+const title = useTitle();
+title.value = "Bench - Sign up";
+</script>
+<template>
+  <div class="flex h-full flex-col bg-white pb-12">
+    <FatHeader>
+      <template v-slot:left>
+        <HomeButton />
+      </template>
+      <template v-slot:right> </template>
+    </FatHeader>
+    <div class="mx-auto mt-20 w-72 text-center lg:mt-32">
+      <div class="flex flex-row items-baseline justify-center gap-1">
+        <div class="font-mono text-2xl font-bold">
+          <span class="-mx-0.5 text-gray-900">[</span>
+          <span class="text-3xl text-orange-600">x</span>
+          <span class="-mx-0.5 text-gray-900">]</span>
+        </div>
+        <h3 class="font-mono text-2xl font-bold">Bench</h3>
+      </div>
+      <h1 class="mt-4 text-5xl font-bold">Welcome</h1>
+      <p class="mt-4 text-lg text-orange-700">AI is yours for the making.</p>
+
+      <div class="mt-10">Username</div>
+    </div>
+  </div>
+</template>
