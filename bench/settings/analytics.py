dc982e49e46d0ea09dfc7eb612c2c18c49486262
Florian Cäsar <florian.caesar@pm.me>
2023-02-28 17:45:40 +0100

    chore: configure basic posthog/sentry integration

--- bench/settings/analytics.py
+++ bench/settings/analytics.py
@@ -1,33 +1,12 @@
-import logging
-import os
-
 import posthog
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.logging import LoggingIntegration
 
 from bench.settings import TEST
-
+from bench.utils.analytics import init_sentry
 
 # Sentry
-def init_sentry():
-    if not TEST:
-        sentry_sdk.utils.MAX_STRING_LENGTH = 10_000_000
-        # https://docs.sentry.io/platforms/python/
-        sentry_logging = LoggingIntegration(level=logging.INFO, event_level=None)
-        sentry_sdk.init(
-            dsn=os.environ["SENTRY_DSN"],
-            environment=os.getenv("SENTRY_ENVIRONMENT", "production"),
-            integrations=[DjangoIntegration(), sentry_logging],
-            request_bodies="always",
-            sample_rate=1.0,
-            send_default_pii=True,
-            traces_sampler=True,
-        )
 
-
-init_sentry()
-
+if not TEST:
+    init_sentry()
 
 # Posthog
 posthog.project_api_key = "phc_d8mi3OMdtKSVA8kzHbBoKtYU3ZsMQakAiLpuOn3W9ma"
