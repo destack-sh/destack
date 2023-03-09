d766924ce7afd49382bddac6955ebf0acd731d78
Florian Cäsar <florian.caesar@pm.me>
2023-03-09 21:51:13 +0100

    feat: focus symbols from explorer

--- frontend/src/components/panels/SymbolExplorer.vue
+++ frontend/src/components/panels/SymbolExplorer.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { StatementType } from "@/gql/graphql";
 import { SYMBOL_TYPE_KEYWORD, useEditorState } from "@/state/editor";
-import { useCurrentModuleRuntime } from "@/state/runtime";
+import { fileOf, useCurrentModuleRuntime } from "@/state/runtime";
 import { computed } from "vue";
 
 const runtime = useCurrentModuleRuntime();
@@ -27,17 +27,25 @@ const allSymbols = computed(() => {
   return symbols;
 });
 
+function focusSymbol(symbol: InterpSymbol) {
+  const file = fileOf(symbol);
+  if (!file) return;
+  editor.focusFile(file as any);
+  editor.editElement(symbol as any);
+}
+
 defineExpose({
   count: computed(() => allSymbols.value.length),
 });
 </script>
 <template>
-  <ul role="list" class="flex flex-col gap-1 py-1 text-sm">
+  <ul role="list" class="flex flex-col py-1 text-sm">
     <li
       v-for="symbol in allSymbols"
       :key="symbol.id"
-      class="flex flex-row gap-1 px-3 text-gray-700"
+      class="flex flex-row gap-1 py-0.5 px-3 text-gray-700 hover:cursor-pointer hover:bg-orange-50 hover:text-gray-900"
       :class="{ 'border-l-2 border-orange-200 pl-2.5': symbol.generated }"
+      @click="focusSymbol(symbol)"
     >
       <span class="text-gray-500">{{ SYMBOL_TYPE_KEYWORD[symbol.symbolType] }}</span>
       <span class="">{{ symbol.name }}</span>
