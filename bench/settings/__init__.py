ee247df53bd8a2c340212d94e3c4c6e08b61d195
Florian Cäsar <florian.caesar@pm.me>
2023-03-10 13:53:21 +0100

    chore: scaffold nats dependencies

--- bench/settings/__init__.py
+++ bench/settings/__init__.py
@@ -17,4 +17,4 @@ from bench.settings.databases import *  # noqa: F401,E402,F403
 from bench.settings.web import *  # noqa: F401,E402,F403
 from bench.settings.logging import *  # noqa: F401,E402,F403
 from bench.settings.analytics import *  # noqa: F401,E402,F403
-from bench.settings.zmq import *  # noqa: F401,E402,F403
+from bench.settings.msg import *  # noqa: F401,E402,F403
