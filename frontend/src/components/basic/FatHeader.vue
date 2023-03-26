2ee6c1d8e5f9e86eaa39307a856c65b25f7ba78e
Florian Cäsar <florian.caesar@pm.me>
2023-03-26 17:51:37 +0200

    chore: scaffold bench instruction metrics

--- frontend/src/components/basic/FatHeader.vue
+++ frontend/src/components/basic/FatHeader.vue
@@ -7,13 +7,18 @@
     (all across the codebase, with slightly different % opacity) 
   -->
   <header
-    class="static mx-auto flex w-full flex-shrink-0 justify-between gap-8 overflow-y-visible border-b border-orange-900 border-opacity-[16%] bg-white"
+    class="static mx-auto flex w-full flex-shrink-0 flex-row justify-between gap-8 overflow-y-visible border-b border-orange-900 border-opacity-[16%] bg-white"
   >
     <!-- Left side -->
     <div class="static flex items-center">
       <slot name="left" />
     </div>
-    <div class="flex min-w-fit flex-shrink-0 items-center justify-end">
+    <!-- Center -->
+    <div class="self-middle flex h-full items-center"></div>
+    <!-- Right side -->
+    <div class="self-middle flex min-w-fit flex-shrink-0 items-center justify-self-end">
+      <!-- TODO @UX: put center in center (properly) -->
+      <slot name="center" />
       <slot name="right" />
     </div>
   </header>
