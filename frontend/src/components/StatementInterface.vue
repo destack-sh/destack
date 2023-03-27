d4ddacb538b3fa1989d5062924bfdfb6cb4e8ef4
Florian Cäsar <florian.caesar@pm.me>
2023-03-27 13:42:57 +0200

    fix: center fat header properly, apply inline value edit immediately on escape

--- frontend/src/components/StatementInterface.vue
+++ frontend/src/components/StatementInterface.vue
@@ -382,7 +382,7 @@ const inlineActions = computed(() => {
           <button
             v-for="action in inlineActions"
             :key="action.label"
-            class="p-0.5 text-gray-700 hover:bg-orange-100 hover:text-gray-900"
+            class="p-0.5 text-gray-500 hover:bg-orange-100 hover:text-gray-800"
             @click="action.action"
           >
             <component :is="action.icon" class="h-4 w-4" />
