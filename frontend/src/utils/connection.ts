6ff91122a31785d1afc27dd8f2cd61cb262674ba
Florian Cäsar <florian.caesar@pm.me>
2023-04-10 17:02:33 +0200

    fix: error if connection edges are retained without limit

--- frontend/src/utils/connection.ts
+++ frontend/src/utils/connection.ts
@@ -8,9 +8,13 @@ type Connection<T> = {
 
 export function getUpdatedConnectionQuery<T>(
   node: T & { id: string },
-  prev?: Connection<T>,
-  maxLength?: number
+  prev: Connection<T> | undefined,
+  maxLength: number
 ): Connection<T> {
+  if (!maxLength) {
+    throw new Error("maxLength must be set (else thy memory shall be eaten)");
+  }
+
   // cursor is base64-encoded Connection:{nodeId}
   const newEdge = {
     __typename: "NodeEdge",
