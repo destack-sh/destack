5a197fa14c98207769f0d308006b38d9bbfa75da
Florian Cäsar <florian.caesar@pm.me>
2023-04-12 14:59:27 +0200

    chore: improve inline value cell ergonomics

--- frontend/src/utils/shortcuts.ts
+++ frontend/src/utils/shortcuts.ts
@@ -36,10 +36,17 @@ export function applyShortcuts() {
   // supprres undo/redo actions if mousetrap-no-do class is present
   // this is a custom implementation of Mousetrap.stopCallback, see https://craig.is/killing/mice
   const suppressedUndoRedoShortcuts = ["ctrl+z", "ctrl+shift+z"];
+  const suppressedTabShortcuts = ["tab", "shift+tab"];
   Mousetrap.prototype.stopCallback = function (e: any, element: HTMLElement, combo: string) {
     // If the element has the class "mousetrap-no-do", stop the callback for the specified shortcuts
     if ((" " + element.className + " ").indexOf(" mousetrap-no-do ") > -1) {
       if (suppressedUndoRedoShortcuts.includes(combo)) {
+        return true;
+      }
+    }
+    // If the element has the class "mousetrap-no-tab", stop the callback for the specified shortcuts
+    if ((" " + element.className + " ").indexOf(" mousetrap-no-tab ") > -1) {
+      if (suppressedTabShortcuts.includes(combo)) {
         return true;
       }
     }
