31931dc309fd007fc4cb88fecf03425d72408661
Florian Cäsar <florian.caesar@pm.me>
2023-04-19 09:42:35 +0200

    fix: handle visual glitch for narrow fat header

--- frontend/src/components/basic/FatHeader.vue
+++ frontend/src/components/basic/FatHeader.vue
@@ -7,7 +7,7 @@
     (all across the codebase, with slightly different % opacity) 
   -->
   <header
-    class="static mx-auto flex w-full grid-cols-3 flex-row gap-8 overflow-y-visible border-b border-orange-900 border-opacity-[16%] bg-white 2xl:grid"
+    class="static mx-auto flex w-full grid-cols-3 flex-row justify-between gap-8 overflow-y-visible border-b border-orange-900 border-opacity-[16%] bg-white 2xl:grid"
   >
     <!-- Left side -->
     <div class="static flex items-center">
