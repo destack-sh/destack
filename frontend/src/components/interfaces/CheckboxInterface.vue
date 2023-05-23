392ee138650daf280aacb3a0c1b5d0c607eb0894
Florian Cäsar <florian.caesar@pm.me>
2023-05-23 07:47:50 +0200

    fix: add focus/blur to bool interfaces

--- frontend/src/components/interfaces/CheckboxInterface.vue
+++ frontend/src/components/interfaces/CheckboxInterface.vue
@@ -11,19 +11,24 @@ const emit = defineEmits<{
   (e: "update:modelValue", value: boolean): void;
 }>();
 
+const checkboxRef = ref<HTMLInputElement | null>(null);
+
 defineExpose({
   click: () => {
     if (!props.readonly) {
       emit("update:modelValue", !props.modelValue);
     }
   },
+  focus: () => checkboxRef.value?.focus(),
+  blur: () => checkboxRef.value?.blur(),
 });
 </script>
 <template>
   <div>
-    <!-- Wrapper element to forward appropriate properties -->
+    <!-- Wrapper element to forward appropriate properties only -->
     <input
       type="checkbox"
+      ref="checkboxRef"
       class="h-4 w-4 rounded border border-gray-300 text-orange-600 ring-0 hover:cursor-pointer focus:ring-0"
       :checked="modelValue"
       @change="
