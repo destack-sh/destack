a52b93d0af4cc0d09d7119cdc178f9f72b3c87e0
Florian Cäsar <florian.caesar@pm.me>
2023-06-17 17:09:02 +0200

    fix: ignore issue update for deleted subjects

--- frontend/src/components/statements/ModifierCell.vue
+++ frontend/src/components/statements/ModifierCell.vue
@@ -1,11 +1,7 @@
 <script lang="ts" setup>
 import { useStatementContext } from "@/state/statement";
-import { MODIFIER_KEYWORD } from "@/state/type";
-import { computed } from "vue";
-
 const context = useStatementContext();
-const modifierKeyword = computed(() => MODIFIER_KEYWORD[context.statement.value.modifier]);
 </script>
 <template>
-  <span class="text-orange-600" :contenteditable="false">{{ modifierKeyword }}</span>
+  <span class="text-orange-600" :contenteditable="false">{{ context.statement.value.modifier }}</span>
 </template>
