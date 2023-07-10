b6f509ebc62a914ef3d13bb3206d09825246990e
Florian Cäsar <florian.caesar@pm.me>
2023-07-10 09:51:18 +0200

    chore: update icon

--- frontend/src/components/basic/HomeButton.vue
+++ frontend/src/components/basic/HomeButton.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup></script>
 <template>
   <div class="group flex flex-shrink-0 items-center">
-    <RouterLink to="/" class="px-[13px] py-2 hover:bg-orange-100">
-      <div class="font-mono text-lg font-bold">
-        <span class="-mx-0.5 inline-block translate-x-0 text-gray-900 transition-transform group-hover:-translate-x-0.5"
-          >[</span
-        >
-        <span class="text-xl text-orange-600">x</span>
-        <span class="-mx-0.5 inline-block translate-x-0 text-gray-900 transition-transform group-hover:translate-x-0.5"
-          >]</span
-        >
+    <RouterLink to="/" class="px-[13px] py-2.5 hover:bg-orange-100">
+      <div class="h-6 w-6 transform transition-transform duration-150 hover:scale-110">
+        <svg viewBox="0 0 300 300">
+          <rect x="0" y="0" width="100" height="100" fill="#ea580c" rx="15" ry="15" />
+          <rect x="200" y="0" width="100" height="100" fill="#ea580c" rx="15" ry="15" />
+          <rect x="100" y="100" width="100" height="100" fill="#ea580c" rx="15" ry="15" />
+          <rect x="0" y="200" width="100" height="100" fill="#ea580c" rx="15" ry="15" />
+          <rect x="200" y="200" width="100" height="100" fill="#ea580c" rx="15" ry="15" />
+        </svg>
       </div>
     </RouterLink>
   </div>
