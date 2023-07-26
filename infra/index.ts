07c8667c755129cf16f5605d751b111f0589a87e
Florian Cäsar <florian.caesar@pm.me>
2023-07-26 17:20:57 +0200

    chore(infra): set LOCAL_ENV in prod

--- infra/index.ts
+++ infra/index.ts
@@ -385,6 +385,10 @@ const PUBLIC_BACKEND_VARS = [
     value: config.require("env"),
   },
   {
+    name: "LOCAL_ENV", // should probably merge this with ENVIRONMENT
+    value: config.require("env"),
+  },
+  {
     name: "NOISY_LOG_LEVEL",
     value: "DEBUG",
   },
