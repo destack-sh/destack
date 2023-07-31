208ea12581780222dedcbc7048a0d896328c5241
Florian Cäsar <florian.caesar@pm.me>
2023-07-31 10:47:05 +0200

    chore: tighten/adjust Bench spacing

--- frontend/src/components/basic/HomeButton.vue
+++ frontend/src/components/basic/HomeButton.vue
@@ -3,7 +3,7 @@ import BenchIcon from "@/components/basic/BenchIcon.vue";
 </script>
 <template>
   <div class="group flex flex-shrink-0 items-center">
-    <RouterLink to="/" class="group px-[15px] py-2 hover:bg-orange-100">
+    <RouterLink to="/" class="group px-[11px] py-[8px] hover:bg-orange-100">
       <BenchIcon class="h-5 w-5 transform transition-transform duration-150 group-hover:scale-110" />
     </RouterLink>
   </div>
