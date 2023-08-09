d57a452a6e9e69c4fa1cc369e09f6f2d7333ed92
Florian Cäsar <florian.caesar@pm.me>
2023-08-09 17:33:40 +0200

    feat: make cron/interval time trigger ui work

--- frontend/src/state/trigger.ts
+++ frontend/src/state/trigger.ts
@@ -19,7 +19,7 @@ import {
 import { DateTime } from "luxon";
 import { ref, watch, type Ref } from "vue";
 import cronInterval from "cron-parser";
-import cronHunaize from "cronstrue";
+import cronHumanize from "cronstrue";
 
 export const TRIGGER_ICONS_OUTLINE: Partial<Record<TriggerType, any>> = {
   [TriggerType.Api]: LinkIconOutline,
@@ -61,6 +61,13 @@ export const INTERVAL_UNITS = {
 export type INTERVAL_UNIT = keyof typeof INTERVAL_UNITS;
 export const OCCURENCES_PREVIEW = 2;
 
+export const VALID_INTERVAL_VALUES_BY_UNIT: Record<INTERVAL_UNIT, { min: number; max: number }> = {
+  minute: { min: 1, max: 60 },
+  hour: { min: 1, max: 24 },
+  day: { min: 1, max: 7 },
+  week: { min: 1, max: 52 },
+};
+
 export type TriggerSchedule = {
   type: ScheduleType;
   timezone: string;
@@ -81,7 +88,8 @@ export function getTriggerIntervalUnit(interval: number): INTERVAL_UNIT {
       return unit as INTERVAL_UNIT;
     }
   }
-  throw new Error(`invalid interval ${interval}`);
+  // invalid, fallback to minute
+  return "minute";
 }
 
 export function getTriggerSchedule(
@@ -98,20 +106,33 @@ export function getTriggerSchedule(
   const nextOccurrencesCount = options?.nextOccurrences ?? OCCURENCES_PREVIEW;
 
   if (scheduleType == ScheduleType.Interval) {
-    // interval
-    valid = true;
-    const intervalUnit = getTriggerIntervalUnit(interval);
-    humanized = `Every ${interval} ${intervalUnit}s`;
-    for (let i = 0; i < nextOccurrencesCount; i++) {
-      nextOccurrences.push(now.plus({ seconds: interval }));
-    }
-    lastOccurrence = now.minus({ seconds: interval });
-  } else if (scheduleType == ScheduleType.Cron) {
-    // cron
-    try {
-      const interval = cronInterval.parseExpression(cron);
+    if (interval <= 1) {
+      valid = false;
+    } else {
       valid = true;
-      humanized = cronHunaize.toString(cron);
+      const intervalUnit = getTriggerIntervalUnit(interval);
+      humanized = `every ${interval / INTERVAL_UNITS[intervalUnit]} ${intervalUnit}s`;
+
+      // TODO @Performance: use more optimal algorithm to find past/next occurrences of interval schedule
+      const nowSeconds = now.toSeconds();
+      let next = TRIGGER_INTERVAL_ORIGIN.toSeconds();
+      let previous = next;
+      while (next < nowSeconds) {
+        previous = next;
+        next = next + interval;
+      }
+      lastOccurrence = DateTime.fromSeconds(previous);
+      for (let i = 0; i < nextOccurrencesCount; i++) {
+        nextOccurrences.push(DateTime.fromSeconds(next));
+        next = next + interval;
+      }
+    }
+  } else if (scheduleType == ScheduleType.Cron) {
+    try {
+      const interval = cronInterval.parseExpression(cron, { tz: trigger.timezone });
+      valid = true;
+      humanized = cronHumanize.toString(cron);
+      humanized = humanized[0].toLowerCase() + humanized.slice(1);
 
       lastOccurrence = DateTime.fromMillis(interval.prev().getTime());
       for (let i = 0; i < nextOccurrencesCount; i++) {
@@ -127,6 +148,7 @@ export function getTriggerSchedule(
   } else {
     throw new Error(`unexpected schedule type ${scheduleType}`);
   }
+
   return {
     type: scheduleType,
     valid,
@@ -140,7 +162,7 @@ export function useTriggerSchedule(
   trigger: Ref<Pick<Trigger, "type" | "scheduleType" | "timezone" | "interval" | "cron"> | null>,
   options?: { nextOccurrences?: number }
 ) {
-  const now = useTimeFromNow(250);
+  const now = useTimeFromNow(60 * 1000);
   const schedule: Ref<TriggerSchedule | null> = ref(null);
 
   watch(
@@ -157,7 +179,7 @@ export function useTriggerSchedule(
       const timezone = timezoneMaybe ?? "UTC";
       const interval = intervalMaybe ?? 60 * 60;
       const cron = cronMaybe ?? "0 * * * *";
-      schedule.value = getTriggerSchedule({ timezone, scheduleType, interval, cron }, now.now.value);
+      schedule.value = getTriggerSchedule({ timezone, scheduleType, interval, cron }, now.now.value, options);
     },
     {
       immediate: true,
