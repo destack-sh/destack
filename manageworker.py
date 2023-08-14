a0f75d545bcff44c424e584313f7d60826aaad1a
Florian Cäsar <florian.caesar@pm.me>
2023-08-14 08:55:31 +0200

    fix: don't init sentry locally for worker

--- manageworker.py
+++ manageworker.py
@@ -12,6 +12,7 @@ from bench.utils.analytics import init_sentry
 from bench.utils.cache import test_redis_connection
 from bench.utils.logging import configure_logging
 from bench.utils.monitoring import restart_on_file_changes
+from bench.utils.utils import DEBUG, LOCAL
 from bench.worker import WorkerNode
 from bench.worker.host import WorkerHost
 
@@ -21,7 +22,8 @@ os.environ["VERSION"] = Path("version").read_text().strip()
 dotenv.load_dotenv(verbose=True)
 configure_logging(apply_logging=True, apply_structlog=True)
 
-init_sentry(django=False)
+if not (LOCAL or DEBUG):
+    init_sentry(django=False)
 
 if os.environ.get("DEBUG") == "1":
     # auto reload on file change if in dev mode
