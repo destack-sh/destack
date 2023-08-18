03d01a34f57c1dd1ee314a9a775fc4a32c27a21f
Florian Cäsar <florian.caesar@pm.me>
2023-08-18 08:48:11 +0200

    fix: log worker mark as active redis failure

--- bench/worker/worker.py
+++ bench/worker/worker.py
@@ -159,7 +159,8 @@ class WorkerNode(Monitored):
         # :WorkerSetActive
         active_key = f"worker_set.{self.worker_set_id}.{self.worker_node_id}.last_active_at"
         self.log.debug("mark_as_active", active_key=active_key)
-        await redis.set(active_key, value=str(time.time()), ex=24 * 60 * 60)
+        if not await redis.set(active_key, value=str(time.time()), ex=24 * 60 * 60):
+            logger.error("mark_as_active.failed", active_key=active_key)
         self.log.debug("mark_as_active.done")
 
     async def mark_as_active_if_active_forever(self, interval: int):
