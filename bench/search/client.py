92ce31458813b288f421b7e3e7666827d7f06a78
Florian Cäsar <florian.caesar@pm.me>
2023-11-14 09:01:56 +0100

    chore: support local worker imitation for debugging auth issues

--- bench/search/client.py
+++ bench/search/client.py
@@ -2,11 +2,11 @@ from opensearchpy import AsyncOpenSearch, OpenSearch
 
 from bench.utils.utils import LOCAL, get_from_env
 
-OS_HOST = get_from_env("LOCAL_OS_HOST", default="localhost", alt="GLOBAL_OS_HOST")
+OS_HOST = get_from_env("LOCAL_OS_HOST", alt="GLOBAL_OS_HOST")
 OS_NAME = get_from_env("LOCAL_OS_NAME", optional=True)
 OS_PORT = get_from_env("LOCAL_OS_PORT", default=9200, type_cast=int, alt="GLOBAL_OS_PORT")
-OS_USERNAME = get_from_env("LOCAL_OS_USERNAME", default="admin", alt="GLOBAL_OS_USERNAME")
-OS_PASSWORD = get_from_env("LOCAL_OS_PASSWORD", default="admin", alt="GLOBAL_OS_PASSWORD")
+OS_USERNAME = get_from_env("LOCAL_OS_USERNAME", alt="GLOBAL_OS_USERNAME")
+OS_PASSWORD = get_from_env("LOCAL_OS_PASSWORD", alt="GLOBAL_OS_PASSWORD")
 
 os_client_sync = OpenSearch(
     hosts=[{"host": OS_HOST, "port": OS_PORT}],
--- manageworker.py
+++ manageworker.py
@@ -7,37 +7,46 @@ from uuid import UUID
 import dotenv
 import structlog
 
-os.environ["VERSION"] = Path("version").read_text().strip()  # must come first
+# must come first
+os.environ["VERSION"] = Path("version").read_text().strip()
+dotenv.load_dotenv(verbose=True)
+dotenv.load_dotenv(".env.worker", verbose=True)
+
+from bench.utils.logging import configure_logging  # noqa: E402
+
+logger = structlog.get_logger(__name__)
+configure_logging(apply_logging=True, apply_structlog=True)
 
 from bench.msg.core import init_nats, process_soon_queue  # noqa: E402
 from bench.utils.analytics import init_sentry  # noqa: E402
 from bench.utils.cache import test_redis_connection  # noqa: E402
-from bench.utils.logging import configure_logging  # noqa: E402
 from bench.utils.monitoring import restart_on_file_changes  # noqa: E402
 from bench.utils.utils import DEBUG, LOCAL  # noqa: E402
 from bench.worker import WorkerNode  # noqa: E402
 from bench.worker.host import WorkerHost  # noqa: E402
 
-logger = structlog.get_logger(__name__)
-
-dotenv.load_dotenv(verbose=True)
-configure_logging(apply_logging=True, apply_structlog=True)
-
 if not (LOCAL or DEBUG):
     init_sentry(django=False)
 
-if os.environ.get("DEBUG") == "1":
+if os.environ.get("DEBUG") == "1" and "WORKER_SET_ID" not in os.environ:
     # auto reload on file change if in dev mode
     worker_set_id = None
     worker_node_id = "local"
     project_id = None
     nats_name = "worker-local"
+    logger.info("worker.dev_mode")
 else:
     # production mode, one worker per process
     worker_set_id = UUID(os.environ["WORKER_SET_ID"])
     worker_node_id = os.environ["WORKER_NODE_ID"].replace(".", "-")
     project_id = UUID(os.environ["WORKER_PROJECT_ID"])
     nats_name = f"worker-{worker_set_id}-{worker_node_id}"
+    logger.info(
+        "worker.prod_mode",
+        worker_set_id=worker_set_id,
+        worker_node_id=worker_node_id,
+        project_id=project_id,
+    )
 
 
 async def _run_node():
@@ -65,13 +74,6 @@ async def _run_host():
     await host.run_forever()
 
 
-async def _run_module(path: str, entrypoints: list[str]):
-    await init_nats(nats_name)
-    await test_redis_connection()
-    raise NotImplementedError("not yet supported")
-
-
-# run node if arg1 is 'worker', run host if arg1 is 'host'
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise RuntimeError("missing argument")
@@ -79,9 +81,5 @@ if __name__ == "__main__":
         asyncio.run(_run_host())
     elif sys.argv[1] == "worker":
         asyncio.run(_run_node())
-    elif sys.argv[1] == "run":
-        if len(sys.argv) < 4:
-            raise RuntimeError("run <path> <entrypoint> [<entrypoint> ...]")
-        asyncio.run(_run_module(sys.argv[2], sys.argv[3:]))
     else:
         raise RuntimeError(f"invalid arguments: {sys.argv}")
