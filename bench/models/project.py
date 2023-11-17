acec71ed9c9fbba9fbed06bcdbfdb1bb1f51d697
Florian Cäsar <florian.caesar@pm.me>
2023-11-17 09:32:37 +0100

    feat: create local pg database on project create

--- bench/models/project.py
+++ bench/models/project.py
@@ -96,11 +96,14 @@ class ProjectManager(models.Manager["Project"]):
         # infra
         if create_infra:
             from bench.server.search import create_local_os_index
+            from bench.server.sql import create_local_pg_database
 
             logger.info("project.create_infra", project=project)
             start_time = datetime.now()
             create_local_worker_set(project, upsert=False)
             create_local_os_index(project, upsert=False)
+            async_to_sync(create_local_pg_database)(project, upsert=False)
+
             duration = datetime.now() - start_time
             logger.info(
                 "project.create_infra.done", project=project, duration=duration.total_seconds()
