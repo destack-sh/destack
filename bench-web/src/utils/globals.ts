a0d5e7667d3caa038a52b96bf4590aa5c74c2dbc
Florian Cäsar <florian.caesar@pm.me>
2024-02-28 21:38:43 +0100

    chore: scaffold grpc web client services

--- bench-web/src/utils/globals.ts
+++ bench-web/src/utils/globals.ts
@@ -3,4 +3,4 @@ export const VERSION = import.meta.env.VITE_APP_VERSION;
 export const COMMIT = import.meta.env.VITE_APP_GIT_COMMIT || "dev";
 
 export const IS_DEBUG = import.meta.env.DEV;
-export const SUPERVISOR_URL = import.meta.env.VITE_APP_SUPERVISOR_URL || "http://localhost:50051";
+export const SUPERVISOR_URL = import.meta.env.VITE_APP_SUPERVISOR_URL || "http://127.0.0.1:8080";
