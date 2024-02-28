a59bd7a6a0d5f9604df2ab61c4e8a5687326c089
Florian Cäsar <florian.caesar@pm.me>
2024-02-28 13:33:45 +0100

    chore: use uv for venv management

--- scripts/update_requirements.sh
+++ scripts/update_requirements.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # update all requirements files from their .in
-python -m piptools compile requirements.in
-python -m piptools compile requirements-runtime.in
-python -m piptools compile requirements.in requirements-dev.in --output-file requirements-dev.txt
-python -m piptools compile requirements-runtime.in requirements-runtime-dev.in --output-file requirements-runtime-dev.txt
+uv pip compile requirements.in
+uv pip compile requirements-runtime.in
+uv pip compile requirements.in requirements-dev.in --output-file requirements-dev.txt
 
 # optionally also sync packages with --sync
 if [ "$1" == "--sync" ]; then
-    ./venv/bin/python -m piptools sync requirements-dev.txt
-    ./venv-runtime/bin/python -m piptools sync requirements-runtime-dev.txt
+    source ./venv-runtime/bin/activate && uv pip sync requirements-runtime.txt
+    source ./venv/bin/activate && uv pip sync requirements-dev.txt
 fi
