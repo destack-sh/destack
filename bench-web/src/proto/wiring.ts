b22afab308ed347f937a5fe2df0716c487a02e7f
Florian Cäsar <florian.caesar@pm.me>
2024-04-30 15:36:31 +0200

    chore(bench-web): encode/decode type identity key

--- bench-web/src/proto/wiring.ts
+++ bench-web/src/proto/wiring.ts
@@ -85,7 +85,8 @@ export function makeDefaultStruct<T extends StructType>(
   data: Partial<Omit<StructTypeMapping[T], "metatype" | "id">> & { metatype: T },
 ): StructTypeMapping[T] {
   const allProperties = STRUCT_PROPERTY_ENUM_BY_TYPE[data.metatype as unknown as ObjectType];
-  if (allProperties == null) throw new Error(`missing properties for struct type: ${data.metatype} (${typeof data.metatype})`);
+  if (allProperties == null)
+    throw new Error(`missing properties for struct type: ${data.metatype} (${typeof data.metatype})`);
   const messageType = MESSAGE_TYPE_BY_OBJECT_TYPE[data.metatype as unknown as ObjectType]!;
   let ord = 1; // skip metatype
   const struct = { ...data } as unknown as StructTypeMapping[T];
@@ -355,4 +356,12 @@ export function unwrapSomeNode(node: SomeNodeData): AnyNodeData {
   const oneOfKind = node.node.oneofKind;
   if (!oneOfKind) throw new Error("missing oneofKind");
   return (node.node as any)[oneOfKind];
+}
+
+export function toRobustJson(value: any): any {
+  // nocheckin: toRobustJson
+}
+
+export function fromRobustJson(value: any): any {
+  // nocheckin: fromRobustJson
 }
