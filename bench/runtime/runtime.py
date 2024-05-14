c422b4318331049591febac62c78952ffa04691d
Florian Cäsar <florian.caesar@pm.me>
2024-05-14 10:58:57 +0200

    chore: scaffold new Runtime

--- bench/runtime/runtime.py
+++ bench/runtime/runtime.py
@@ -1,22 +1,25 @@
-from subprocess import Popen
+from urllib.parse import urlparse
 from uuid import UUID
 
 import structlog
+from grpclib.client import Channel
 
 from bench.proto.services import MonitoredServiceBase
-from bench.proto.wire import RuntimeBase
+from bench.proto.wire import RuntimeBase, SupervisorStub
 
 logger = structlog.get_logger(__name__)
 
 
 class Runtime(RuntimeBase, MonitoredServiceBase):
     """
-    A runtime processes selected Runs for a Bench/Package in Sessions.
+    A Runtime processes selected Runs for a Bench/Package in Sessions.
+    A Runtime process is started for each active Package in a Bench.
     """
 
     def __init__(
         self,
         *,
+        supervisor_url: str,
         client_id: UUID | None = None,
         user_id: UUID | None = None,
         server_id: UUID | None = None,
@@ -24,12 +27,20 @@ class Runtime(RuntimeBase, MonitoredServiceBase):
         package_id: UUID | None = None,
     ):
         super().__init__()
+        # parse out supervisor host and port
+        _supervisor_url = urlparse(supervisor_url)
+        self.supervisor_host = _supervisor_url.hostname
+        self.supervisor_port = _supervisor_url.port
+        if self.supervisor_host is None or self.supervisor_port is None:
+            raise ValueError(f"invalid supervisor URL: {supervisor_url}")
+        self.supervisor = SupervisorStub(Channel(self.supervisor_host, self.supervisor_port))
+
+        # context
         self.client_id = client_id
         self.server_id = server_id
         self.user_id = user_id
         self.bench_id = bench_id
         self.package_id = package_id
-        self.processes: dict[UUID, Popen] = {}
 
     def __str__(self):
         return f"{self.bench_id} {self.server_id}"
