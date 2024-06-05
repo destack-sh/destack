f2e16bd285d22695c8f3a53cee106899f1fd1f36
Florian Cäsar <florian.caesar@pm.me>
2024-06-05 18:24:06 +0200

    feat(bench-web): make node path nodes draggable

--- bench-web/src/views/builtins/NodePath.vue
+++ bench-web/src/views/builtins/NodePath.vue
@@ -4,6 +4,7 @@ import type { NodeReferenceData } from "@/proto/wire";
 import type { ReadNodeGraph } from "@/system/graph";
 import { IconInline, getNodeIcon } from "@/system/icon";
 import { canvas } from "@/system/space";
+import { startDragging } from "@/utils/drag";
 import { computed } from "vue";
 
 const props = defineProps<{
@@ -22,15 +23,19 @@ const path = computed(() => ancestors.value.slice().reverse());
   <!-- Breadcrumb -->
   <div class="flex flex-row items-center gap-x-1 truncate">
     <template v-for="(node, i) in path" :key="i">
-      <span
+      <!-- Node -->
+      <button
         class="flex cursor-pointer flex-row items-center rounded px-1 hover:bg-primary-100 hover:text-primary-900"
         role="button"
         :class="node.id == self?.id || node.id == focus?.id ? 'text-primary-900' : 'text-gray-600'"
+        :draggable="true"
         @click.stop="canvas.goToNode(node)"
+        @dragstart.stop="(e: DragEvent) => startDragging(e, graph, node)"
       >
         <IconInline v-bind="getNodeIcon(node)" class="mr-1.5" />
         <span class="">{{ node.name }}</span>
-      </span>
+      </button>
+      <!-- Separator -->
       <i v-if="i < path.length - 1" class="fas fa-chevron-right text-gray-400" />
     </template>
   </div>
