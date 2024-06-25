73866a80abfcd358a2a6309d9997b8f007f41b3f
Florian Cäsar <florian.caesar@pm.me>
2024-06-25 09:17:11 +0200

    test: assign unique nonce to every remote session

--- bench/proto/services.py
+++ bench/proto/services.py
@@ -202,7 +202,7 @@ class ServiceBase:
                         response_stream = func(request)
                     span.end()  # end early (streaming, span shouldn't continue forever)
                     async for response in response_stream:
-                        log.trace(f"{rpc_name}.stream", response=response)
+                        log.trace(f"{rpc_name}.update", response=response)
                         await stream.send_message(response)
                 else:
                     raise NotImplementedError(f"unsupported cardinality {cardinality}")
