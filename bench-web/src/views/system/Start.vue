09f338a0f5aca76b395cb748b249a60350af27c9
Florian Cäsar <florian.caesar@pm.me>
2024-06-26 16:34:46 +0200

    fix(bench-web): cache connections properly

--- bench-web/src/views/system/Start.vue
+++ bench-web/src/views/system/Start.vue
@@ -68,9 +68,8 @@ defineExpose<ViewExposed>({ self });
         <!-- Controls -->
         <div class="ml-auto flex flex-row items-center pl-1.5">
           <button
-            v-tooltip="{ title: 'Start run' }"
             :disabled="runnableNode == null"
-            class="h-fit hover:text-primary-900 enabled:text-gray-700 disabled:text-gray-400"
+            class="h-fit enabled:hover:text-primary-900 enabled:text-gray-700 disabled:text-gray-400"
             @click="
               () => {
                 if (runnableNode == null) return;
@@ -79,7 +78,8 @@ defineExpose<ViewExposed>({ self });
               }
             "
           >
-            <i class="fas fa-play w-5 text-center" />
+          <i class="fas fa-play w-5 text-center" />
+          <span class="ml-0.5">Start</span>
           </button>
         </div>
       </div>
