e5c872f41987cd6f6d5f0dbac47a0d8cede59855
Florian Cäsar <florian.caesar@pm.me>
2024-08-03 14:41:31 +0200

    test(bench-web): fix value packing / object fabrication with rich references

--- bench-web/src/language/value.test.ts
+++ bench-web/src/language/value.test.ts
@@ -1,13 +1,13 @@
-import { BenchType, EnumType, NodeType, ObjectType, PrimitiveType, StructType, TypeKind } from "@/proto/wire";
 import { fabricate } from "@/language/graph.test";
 import { OBJECT_TYPES, getTkFromPtrMaybe } from "@/language/utils";
 import {
-  packBuiltinObject,
-  unpackBuiltinObject,
   decodeTypeIdentity,
   encodeTypeIdentity,
+  packBuiltinObject,
+  unpackBuiltinObject,
   type TypeIdentity,
 } from "@/language/value";
+import { BenchType, NodeType, ObjectType, PrimitiveType, TypeKind } from "@/proto/wire";
 import { describe, expect, test } from "vitest";
 
 // the test data & targets are from the backend bench implementation
