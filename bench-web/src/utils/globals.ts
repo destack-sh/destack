c634295e3bf12bdc59b909278056447f3d8f865d
Florian Cäsar <florian.caesar@pm.me>
2024-08-08 10:31:22 +0200

    chore: use proper grpc health service protocol

--- bench-web/src/utils/globals.ts
+++ bench-web/src/utils/globals.ts
@@ -1,7 +1,7 @@
 import { pretendReadonly } from "@/utils/ref";
 import { ref } from "vue";
 
-export const VERSION = "2024.08.08.1";
+export const VERSION = "2024.08.08.2";
 
 // global constants given from env :BenchWebEnv
 
