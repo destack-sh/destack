759a5e66b8d5dbb01ba587bf17a5dc000446256a
Florian Cäsar <florian.caesar@pm.me>
2024-08-08 15:00:55 +0200

    refactor: move requirements into bench top-level directory

--- scripts/update_requirements.sh
+++ scripts/update_requirements.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # update all requirements files from their .in
-uv pip compile requirements/system.in --output-file requirements/system.txt
-uv pip compile requirements/system.in requirements/dev.in --output-file requirements/system-dev.txt
-uv pip compile requirements/runtime.in --output-file requirements/runtime.txt
-uv pip compile requirements/runtime.in requirements/dev.in --output-file requirements/runtime-dev.txt
+uv pip compile bench/requirements/system.in --output-file bench/requirements/system.txt
+uv pip compile bench/requirements/system.in bench/requirements/dev.in --output-file bench/requirements/system-dev.txt
+uv pip compile bench/requirements/runtime.in --output-file bench/requirements/runtime.txt
+uv pip compile bench/requirements/runtime.in bench/requirements/dev.in --output-file bench/requirements/runtime-dev.txt
 
 # optionally also sync packages with --sync
 if [ "$1" == "--sync" ]; then
-    source ./venv-runtime/bin/activate && uv pip sync requirements/runtime-dev.txt
-    source ./venv/bin/activate && uv pip sync requirements/system-dev.txt
+    source ./venv-runtime/bin/activate && uv pip sync bench/requirements/runtime-dev.txt
+    source ./venv/bin/activate && uv pip sync bench/requirements/system-dev.txt
 fi
