0dd0e262a2000aa4704496e5e929f04fd331b2e7
Florian Cäsar <florian.caesar@pm.me>
2024-08-12 16:59:38 +0200

    refactor: move infra to bench-infra

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # deploy with terraform
-terraform -chdir=infra apply -auto-approve -var-file=prod.tfvars
+terraform -chdir=bench-infra apply -auto-approve -var-file=prod.tfvars
 
 # invalidate cloudfront cache
-aws cloudfront create-invalidation --distribution-id $(terraform -chdir=infra output -json | jq -r '.bench_web_distribution_id.value') --paths "/*" || true
+aws cloudfront create-invalidation --distribution-id $(terraform -chdir=bench-infra output -json | jq -r '.bench_web_distribution_id.value') --paths "/*" || true
