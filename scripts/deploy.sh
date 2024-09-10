709ee6e25849027dd1b9917a4940efc8286dea4e
Florian Cäsar <florian.caesar@pm.me>
2024-09-10 13:51:40 +0200

    infra: impute bench-web environment variables properly in new vite build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ set -e
 terraform -chdir=bench-infra apply -auto-approve -var-file=prod.tfvars
 
 # invalidate cloudfront cache
-aws cloudfront create-invalidation --distribution-id $(terraform -chdir=bench-infra output -json | jq -r '.bench_web_distribution_id.value') --paths "/*" || true
+aws cloudfront create-invalidation --distribution-id $(terraform -chdir=bench-infra output -json | jq -r '.bench_web_distribution_id.value') --paths "/*" >/dev/null || true
