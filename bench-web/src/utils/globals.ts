20b13c362d2b6ad4ae189d2c69f00ac84a21dbf9
Florian Cäsar <florian.caesar@pm.me>
2024-11-01 15:19:52 +0100

    chore: support strict Actions (with Code)

--- bench-web/src/utils/globals.ts
+++ bench-web/src/utils/globals.ts
@@ -2,7 +2,7 @@ import { SpaceCanvas } from "@/ui/space";
 import { createDeferredProxy, pretendReadonly } from "@/utils/ref";
 import { ref } from "vue";
 
-export const VERSION = "2024.11.01.0";
+export const VERSION = "2024.11.01.1";
 
 // global environment variables :BenchWebEnv
 export const COMMIT = import.meta.env.VITE_COMMIT;
