542d1edc019a1815b268198c7f5f260cc0a5d451
Florian Cäsar <florian.caesar@pm.me>
2024-11-08 22:27:34 +0100

    feat: run basic adaptive Actions

--- bench/test/unit/runtime/action.py
+++ bench/test/unit/runtime/action.py
@@ -6,6 +6,7 @@ from bench.language.const import RunStatus
 from bench.language.field import Field
 from bench.language.run import ModelProvider, RunErrorType
 from bench.language.text import md
+from bench.runtime.core import ModelIncapableError
 from bench.test.unit.conftest import RuntimeHandle
 
 TEST_MODEL_PROVIDERS = (ModelProvider.OPENAI, ModelProvider.ANTHROPIC)
@@ -17,40 +18,32 @@ def _for_every_provider():
     )
 
 
-async def test_run_action_empty_strict(local_runtime: RuntimeHandle):
+async def test_run_action_empty_strict(hosted_runtime: RuntimeHandle):
     """Running an empty action in strict mode should raise an error."""
     ActionBlock1 = Block.new(
         ActionBlock,
         "Action1",
         mode=ActionMode.STRICT,
     )
-    local_runtime.page().blocks.append(ActionBlock1)
-    await local_runtime.commit()
+    hosted_runtime.page().blocks.append(ActionBlock1)
+    await hosted_runtime.commit()
 
-    runner = await local_runtime.run(ActionBlock1, return_error=True)
+    runner = await hosted_runtime.run(ActionBlock1, return_error=True)
     assert runner.status == RunStatus.FAILED
     assert runner.error and runner.error.type == RunErrorType.RUN_IMPOSSIBLE
 
 
-async def test_run_action_empty_adaptive(local_runtime: RuntimeHandle):
-    """Running an empty action in adaptive mode should do nothing."""
+async def test_run_action_empty_adaptive(hosted_runtime: RuntimeHandle):
+    """Running an empty action in adaptive mode should raise an error."""
     ActionBlock1 = Block.new(ActionBlock, "Action1", mode=ActionMode.ADAPTIVE)
-    local_runtime.page().blocks.append(ActionBlock1)
-    await local_runtime.commit()
+    hosted_runtime.page().blocks.append(ActionBlock1)
+    await hosted_runtime.commit()
 
-    _ = await local_runtime.run(ActionBlock1)
-
-
-async def test_run_action_empty_dynamic(local_runtime: RuntimeHandle):
-    """Running an empty action in dynamic mode should do nothing."""
-    ActionBlock1 = Block.new(ActionBlock, "Action1", mode=ActionMode.DYNAMIC)
-    local_runtime.page().blocks.append(ActionBlock1)
-    await local_runtime.commit()
-
-    _ = await local_runtime.run(ActionBlock1)
+    with pytest.raises(ModelIncapableError):
+        _ = await hosted_runtime.run(ActionBlock1)
 
 
-async def test_run_action_math(local_runtime: RuntimeHandle):
+async def test_run_action_math(hosted_runtime: RuntimeHandle):
     """
     Running an adaptive action with a math implementation should work.
     Changing the action and re-running should change the output.
@@ -63,16 +56,16 @@ async def test_run_action_math(local_runtime: RuntimeHandle):
         mode=ActionMode.ADAPTIVE,
         fields=[Field.input("x", int), Field.output("y", int)],
     )
-    local_runtime.page().blocks.append(ActionBlock1)
-    await local_runtime.commit()
-    runner = await local_runtime.run(ActionBlock1, inputs={"x": 1})
+    hosted_runtime.page().blocks.append(ActionBlock1)
+    await hosted_runtime.commit()
+    runner = await hosted_runtime.run(ActionBlock1, inputs={"x": 1})
     assert runner.outputs and runner.outputs.y == 2
-    runner = await local_runtime.run(ActionBlock1, inputs={"x": 2})
+    runner = await hosted_runtime.run(ActionBlock1, inputs={"x": 2})
     assert runner.outputs and runner.outputs.y == 3
 
     # New: 'Add 2'
     ActionBlock1.text = md("Add 2")
-    runner = await local_runtime.run(ActionBlock1, inputs={"x": 1})
+    runner = await hosted_runtime.run(ActionBlock1, inputs={"x": 1})
     assert runner.outputs and runner.outputs.y == 3
-    runner = await local_runtime.run(ActionBlock1, inputs={"x": 2})
+    runner = await hosted_runtime.run(ActionBlock1, inputs={"x": 2})
     assert runner.outputs and runner.outputs.y == 4
