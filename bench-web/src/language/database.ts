ebaa568157ea16c92d65b6cca9c0c48417846207
Florian Cäsar <florian.caesar@pm.me>
2024-12-07 13:39:27 +0100

    fix(bench-web): support Record-level actions in Database

---
+++ bench-web/src/language/database.ts
@@ -0,0 +1,7 @@
+import { ActionBuiltinId } from "@/ui/action";
+
+export const RECORD_CONTEXT_ACTIONS: ActionBuiltinId[] = [
+  "common.edit.rename",
+  "common.edit.duplicate",
+  "common.edit.delete",
+];
