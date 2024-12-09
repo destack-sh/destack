254647f91f415e9e47af85b594dd56eb4c998062
Florian Cäsar <florian.caesar@pm.me>
2024-12-09 18:45:53 +0100

    fix: maintain consistent references when cloning

--- bench-web/src/views/builtins/NodePath.vue
+++ bench-web/src/views/builtins/NodePath.vue
@@ -42,7 +42,7 @@ const selfIndex = computed(() => path.value.findIndex((node) => node.id == props
         :data-node-type="node.metatype"
         :draggable="true"
         @click.stop="canvas.goToNode(node)"
-        @dragstart.stop="(e: DragEvent) => startDragging(e, graph, node)"
+        @dragstart.stop="(e: DragEvent) => startDragging(e, node)"
       >
         <NodeReference
           :node="node"
