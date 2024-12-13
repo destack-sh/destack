61ecd2a7ddc37b63b5b645ff7dd69e541f400e69
Florian Cäsar <florian.caesar@pm.me>
2024-12-13 10:26:59 +0100

    chore(bench-web): add supergraphIndex to search

--- bench-web/src/views/builtins/Omnibar.vue
+++ bench-web/src/views/builtins/Omnibar.vue
@@ -1,14 +1,21 @@
 <script lang="tsx" setup>
 import { NodeType, Orientation, ViewData, ViewType } from "@/proto/wire";
-import { OMNIBAR_MODES, addAction, fireAction, type ActionBuiltinId, type OmnibarMode } from "@/ui/action";
 import { packagePtr } from "@/system/client";
+import { bench, canvas, hasLocalPkg, pkgGraph, space, spaceGraph } from "@/system/space";
+import { OMNIBAR_MODES, addAction, fireAction, type ActionBuiltinId, type OmnibarMode } from "@/ui/action";
 import { IconInline, makeIcon } from "@/ui/icon";
-import { actionIndex, graphIndex, useSearch, type SearchIndex, type NodeItem, type ActionItem } from "@/ui/search";
-import { bench, spaceGraph, canvas, pkgGraph, space, hasLocalPkg } from "@/system/space";
-import { nowOrNextTick } from "@/utils/functools";
 import { ScrollbarWidth } from "@/ui/layout";
-import { Casing, toCasing } from "@/utils/string";
+import {
+  ACTION_INDEX,
+  graphIndex,
+  useSearch,
+  type ActionItem,
+  type NodeItem,
+  type SearchIndex
+} from "@/ui/search";
 import { Shortcut } from "@/ui/tooltip";
+import { nowOrNextTick } from "@/utils/functools";
+import { Casing, toCasing } from "@/utils/string";
 import Scroll from "@/views/containers/Scroll.vue";
 import { computed, nextTick, ref, watch, type Ref } from "vue";
 
@@ -33,7 +40,7 @@ const indices = computed(() => {
 
   // actions
   if (["everywhere", "actions"].includes(mode.value)) {
-    indices["Actions"] = actionIndex();
+    indices["Actions"] = ACTION_INDEX;
   }
 
   // views
