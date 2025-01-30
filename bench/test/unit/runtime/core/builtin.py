8b1fe1bc65efc5203a0d8490567d5f3dd1dd03fa
Florian Cäsar <florian.caesar@pm.me>
2025-01-30 23:04:30 +0100

    test: remove local_runtime

--- bench/test/unit/runtime/core/builtin.py
+++ bench/test/unit/runtime/core/builtin.py
@@ -3,21 +3,21 @@ from bench.language.builtin import make_builtins
 from bench.test.unit.conftest import RuntimeHandle
 
 
-async def test_sync_builtins(local_runtime: RuntimeHandle) -> None:
+async def test_sync_builtins(hosted_runtime: RuntimeHandle) -> None:
     """Sync the Builtins page."""
-    Builtins = make_builtins(local_runtime.session)
+    Builtins = make_builtins(hosted_runtime.session)
     sync_node(
-        parent=local_runtime.package,
-        old_root=local_runtime.package.blocks.get("Builtins"),
+        parent=hosted_runtime.package,
+        old_root=hosted_runtime.package.blocks.get("Builtins"),
         new_root=Builtins,
     )
-    assert local_runtime.session.tx.has_edits
-    await local_runtime.commit()
+    assert hosted_runtime.session.tx.has_edits
+    await hosted_runtime.commit()
 
     sync_node(
-        parent=local_runtime.package,
-        old_root=local_runtime.package.blocks.get("Builtins"),
+        parent=hosted_runtime.package,
+        old_root=hosted_runtime.package.blocks.get("Builtins"),
         new_root=Builtins,
     )
-    assert not local_runtime.session.tx.has_edits
-    await local_runtime.commit()
+    assert not hosted_runtime.session.tx.has_edits
+    await hosted_runtime.commit()
