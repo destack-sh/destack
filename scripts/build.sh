4921d3d9bf495a42f1e8e99c8b9b33d57453710b
Florian Cäsar <florian.caesar@pm.me>
2025-04-03 16:46:04 +0200

    chore: update all Dockerfiles

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,27 +12,6 @@ GIT_COMMIT=$(git rev-parse --short HEAD)
 # get version from 'version' file
 VERSION=$(cat version)
 
-# build computer images
-COMPUTER_IMAGES=("bench-computer-ubuntu")
-for IMAGE in ${COMPUTER_IMAGES[@]}; do
-  docker build . \
-    --platform linux/arm64 \
-    -f bench-infra/docker/Dockerfile.computer-ubuntu \
-    -t symbolx/$IMAGE:latest \
-    -t symbolx/$IMAGE:$GIT_COMMIT \
-    -t symbolx/$IMAGE:$VERSION \
-    -t ghcr.io/symbolx/$IMAGE:latest \
-    -t ghcr.io/symbolx/$IMAGE:$GIT_COMMIT \
-    -t ghcr.io/symbolx/$IMAGE:$VERSION \
-    --build-arg GIT_COMMIT=$GIT_COMMIT \
-    --build-arg VERSION=$VERSION
-
-  # push to GHCR
-  docker push ghcr.io/symbolx/$IMAGE:latest
-  docker push ghcr.io/symbolx/$IMAGE:$GIT_COMMIT
-  docker push ghcr.io/symbolx/$IMAGE:$VERSION
-done
-
 # build bench-web with :BenchWebEnv placeholders (to be substituted in deploy)
 # (we need to 'set' them explicitly or they will be removed by vite during the build)
 VITE_COMMIT="VITE_COMMIT" \
@@ -41,14 +20,17 @@ VITE_SUPERVISOR_URL="VITE_SUPERVISOR_URL" \
 VITE_IP_API_KEY="VITE_IP_API_KEY" \
 bun run --cwd bench-web build
 
-# build bench images
-# image names
-IMAGES=("bench-system" "bench-computer-runtime")
-for IMAGE in ${IMAGES[@]}; do
+# build all images
+# image names and their corresponding Dockerfiles
+IMAGES=("bench-computer-ubuntu" "bench-system" "bench-computer-runtime")
+DOCKERFILES=("bench-infra/docker/Dockerfile.computer-ubuntu" "bench-infra/docker/Dockerfile.system-bench" "bench-infra/docker/Dockerfile.runtime-bench")
+
+for i in "${!IMAGES[@]}"; do
+  IMAGE="${IMAGES[$i]}"
+  DOCKERFILE="${DOCKERFILES[$i]}"
   docker build . \
     --platform linux/arm64 \
-    --target $IMAGE \
-    -f bench-infra/docker/Dockerfile.system-bench \
+    -f "$DOCKERFILE" \
     -t symbolx/$IMAGE:latest \
     -t symbolx/$IMAGE:$GIT_COMMIT \
     -t symbolx/$IMAGE:$VERSION \
