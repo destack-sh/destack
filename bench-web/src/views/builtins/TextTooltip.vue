5710603fe6670b6fc96a7b85cbe42c5e575f12b7
Florian Cäsar <florian.caesar@pm.me>
2025-04-07 16:54:32 +0200

    feat(bench-web): add TextTooltip icons/shortcuts

--- bench-web/src/views/builtins/TextTooltip.vue
+++ bench-web/src/views/builtins/TextTooltip.vue
@@ -1,37 +1,42 @@
 <script lang="ts" setup>
 import { IconData } from "@/proto/wire";
+import { Command, CommandBuiltinId, getCommand } from "@/ui/command";
 import { IconInline, makeIcon } from "@/ui/icon";
 import { hasTextMark, setTextMark } from "@/ui/prosemirror/editor";
 import { TextMarkType } from "@/ui/prosemirror/schema";
 import { type TooltipProps } from "@/ui/prosemirror/view";
+import { TooltipInfo } from "@/ui/tooltip";
 
 const props = defineProps<TooltipProps>();
 
-type FormatAction = {
+type FormatCommand = {
   id: string | number;
   icon: IconData;
+  command: Command;
   isChecked: () => boolean;
   toggle: () => void;
 };
 
-function markFormatAction(mark: TextMarkType, icon: string): FormatAction {
-  const action: FormatAction = {
+function markFormatCommand(mark: TextMarkType, commandId: CommandBuiltinId, icon: string): FormatCommand {
+  const command = getCommand(commandId);
+  const formatCommand: FormatCommand = {
     id: mark,
     icon: makeIcon(icon),
+    command: command,
     isChecked: () => hasTextMark(props.view.state, props.view.state.selection, mark) !== false,
     toggle: () => {
       setTextMark(props.view.state, props.view.state.selection, mark, "toggle", props.view.dispatch);
     },
   };
-  return action;
+  return formatCommand;
 }
 
-const formatActions: FormatAction[] = [
-  markFormatAction("bold", "fas fa-bold"),
-  markFormatAction("italic", "fas fa-italic"),
-  markFormatAction("underline", "fas fa-underline"),
-  markFormatAction("strikethrough", "fas fa-strikethrough"),
-  markFormatAction("code", "fas fa-code"),
+const formatCommands: FormatCommand[] = [
+  markFormatCommand("bold", "text.format.bold", "fas fa-bold"),
+  markFormatCommand("italic", "text.format.italic", "fas fa-italic"),
+  markFormatCommand("underline", "text.format.underline", "fas fa-underline"),
+  markFormatCommand("strikethrough", "text.format.strikethrough", "fas fa-strikethrough"),
+  markFormatCommand("code", "text.format.code", "fas fa-code"),
 ];
 </script>
 <template>
@@ -51,15 +56,20 @@ const formatActions: FormatAction[] = [
       @click.stop.prevent
       @mousedown.stop.prevent
     >
-      <!-- Mark-ish actions -->
+      <!-- Mark-ish commands -->
       <button
-        v-for="action in formatActions"
-        :key="action.id"
+        v-for="command in formatCommands"
+        :key="command.id"
+        v-tooltip="{
+          title: command.command.title,
+          shortcuts: command.command.shortcuts,
+          group: 'text.format',
+        }"
         class="rounded px-1 py-0.5 hover:bg-gray-100"
-        :class="[action.isChecked() ? 'text-primary-700' : '']"
-        @mousedown.stop.prevent="action.toggle()"
+        :class="[command.isChecked() ? 'text-primary-700' : '']"
+        @mousedown.stop.prevent="command.toggle()"
       >
-        <IconInline class="w-5 text-center" v-bind="action.icon" />
+        <IconInline class="w-5 text-center" v-bind="command.icon" />
       </button>
     </div>
   </Transition>
