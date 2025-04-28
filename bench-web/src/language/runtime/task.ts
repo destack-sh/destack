78e81dbf4114b3a6f6f17550b0ead8ee606dd8e7
Florian Cäsar <florian.caesar@pm.me>
2025-04-28 19:40:35 +0200

    feat(bench-web): animate active Tasks

--- bench-web/src/language/runtime/task.ts
+++ bench-web/src/language/runtime/task.ts
@@ -1,6 +1,7 @@
 import { ACTIVE_PROCESS_STATUSES, TERMINAL_PROCESS_STATUSES } from "@/language/core/const";
 import { ReadNodeGraph } from "@/language/core/graph";
 import { Transaction } from "@/language/core/transaction";
+import { isProcessActive, isProcessTerminal } from "@/language/runtime/process";
 import { NodeType, ProcessStatus, TaskData } from "@/proto/wire";
 
 /** Create a Task. */
@@ -10,25 +11,11 @@ export function createTask(tx: Transaction, graph: ReadNodeGraph, options: { tas
   return task;
 }
 
-/** Whether the given task is active */
-export function isTaskActive(task: TaskData): boolean {
-  return ACTIVE_PROCESS_STATUSES.includes(task.status);
-}
-
-/** Whether the given task is terminal */
-export function isTaskTerminal(task: TaskData): boolean {
-  return TERMINAL_PROCESS_STATUSES.includes(task.status);
-}
-
 /** 'Toggle' the Task status (whatever that means in its current state). */
-export function toggleTaskStatus(tx: Transaction, task: TaskData): boolean {
-  if (isTaskActive(task)) {
-    return false;
-  } else if (isTaskTerminal(task)) {
+export function toggleTaskStatus(tx: Transaction, task: TaskData) {
+  if (isProcessActive(task) || isProcessTerminal(task)) {
     tx.update(task, { status: task.ownedByPtr != null ? ProcessStatus.CREATED : ProcessStatus.ASSIGNED });
-    return true;
   } else {
     tx.update(task, { status: ProcessStatus.COMPLETED });
-    return true;
   }
 }
