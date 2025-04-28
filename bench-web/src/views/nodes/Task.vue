72a9951869e7412b63923a3a76fa7e535ebbe7bb
Florian Cäsar <florian.caesar@pm.me>
2025-04-28 19:40:35 +0200

    feat(bench-web): animate active Tasks

--- bench-web/src/views/nodes/Task.vue
+++ bench-web/src/views/nodes/Task.vue
@@ -1,24 +1,12 @@
 <script lang="ts" setup>
-import { makeType, makeTypeConstraint } from "@/language/core/type";
-import { isTaskActive, isTaskTerminal, toggleTaskStatus } from "@/language/runtime/task";
-import {
-  ColorShade,
-  ColorType,
-  NodeReferenceData,
-  NodeType,
-  TaskData,
-  TextLineData,
-  TypeKind,
-  ViewData,
-  ViewType,
-} from "@/proto/wire";
+import { isProcessActive, isProcessTerminal } from "@/language/runtime/process";
+import { toggleTaskStatus } from "@/language/runtime/task";
+import { ColorShade, NodeReferenceData, NodeType, TaskData, TextLineData, ViewData } from "@/proto/wire";
 import { TypedNodeReferenceData } from "@/proto/wiring";
 import { PreparedNodeConnection, useAutoConnection } from "@/system/connection";
 import { canvas } from "@/system/space";
-import { pushPopover } from "@/ui/popover";
-import { getColorHex, getProcessColorHex } from "@/ui/style";
+import { getProcessColorHex } from "@/ui/style";
 import Inaccessible from "@/views/builtin/Inaccessible.vue";
-import NodeReference from "@/views/builtin/NodeReference.vue";
 import { FocusAnchor, type ViewEmits, type ViewExpose } from "@/views/common";
 import NativeInput from "@/views/content/NativeInput.vue";
 import TextLine from "@/views/content/TextLine.vue";
@@ -38,21 +26,12 @@ canvas.registerView(self, id);
 const taskPtr = toRef(props, "nodePtr");
 const { graph, connection } = props.preparedConnection ?? useAutoConnection(taskPtr);
 const task = graph.getRef(taskPtr, { ignoreAncestors: true }) as Ref<TaskData | null>;
-const isActive = computed(() => task.value != null && isTaskActive(task.value));
-const isTerminal = computed(() => task.value != null && isTaskTerminal(task.value));
+const isActive = computed(() => task.value != null && isProcessActive(task.value));
+const isTerminal = computed(() => task.value != null && isProcessTerminal(task.value));
 const hasMeta = computed(() => task.value != null && (task.value.dueAt != null || task.value.ownedByPtr != null));
 
 // view
 const nameRef: Ref<InstanceType<typeof NativeInput> | null> = ref(null);
-const fillState = computed(() => {
-  if (isActive.value) {
-    return "half";
-  } else if (isTerminal.value) {
-    return "full";
-  } else {
-    return "empty";
-  }
-});
 
 function focus(anchor?: FocusAnchor | NodeReferenceData) {
   nameRef.value?.focus?.(anchor ?? "left");
@@ -64,17 +43,17 @@ defineExpose<ViewExpose>({ self, id, focus });
   <div v-if="task" class="group/task flex flex-row items-baseline gap-x-2">
     <!-- Status -->
     <button
-      class="flex h-4 w-4 cursor-pointer flex-row items-center justify-center rounded-sm border border-gray-500 bg-white p-[1px] transition-colors duration-75"
+      class="flex h-4 w-4 flex-row items-center justify-center rounded-sm border bg-white p-[1px] transition-colors duration-75 enabled:cursor-pointer"
       :style="{
-        borderColor: fillState != 'empty' ? getProcessColorHex(task.status, ColorShade.S500) : undefined,
+        borderColor: getProcessColorHex(task.status, ColorShade.S500),
       }"
       @mousedown.stop="toggleTaskStatus(connection.tx, task)"
     >
       <span
         class="inline-block h-full w-full rounded-sm transition-all duration-75"
+        :class="[isActive ? 'animate-task-check-progress' : undefined]"
         :style="{
-          backgroundColor: fillState != 'empty' ? getProcessColorHex(task.status, ColorShade.S500) : undefined,
-          clipPath: fillState == 'full' ? undefined : 'inset(0 0 50% 0)',
+          backgroundColor: isActive || isTerminal ? getProcessColorHex(task.status, ColorShade.S500) : undefined,
         }"
       />
     </button>
@@ -99,3 +78,21 @@ defineExpose<ViewExpose>({ self, id, focus });
   </div>
   <Inaccessible v-else :node="taskPtr" :connection="connection" />
 </template>
+
+<style>
+@keyframes task-check-progress {
+  0% {
+    clip-path: inset(0 0 30% 0);
+  }
+  50% {
+    clip-path: inset(0 0 80% 0);
+  }
+  100% {
+    clip-path: inset(0 0 30% 0);
+  }
+}
+
+.animate-task-check-progress {
+  animation: task-check-progress 3s ease-in-out infinite;
+}
+</style>
