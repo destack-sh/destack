3fccb1c579287dcdae2dabbf6f5c8e85dda5c7af
Florian Cäsar <florian.caesar@pm.me>
2025-05-06 12:00:20 +0200

    chore(bench-web): show Wizard only if not loading

--- bench-web/src/utils/globals.ts
+++ bench-web/src/utils/globals.ts
@@ -1,7 +1,7 @@
 import { pretendReadonly } from "@/utils/ref";
 import { ref } from "vue";
 
-export const VERSION = "2025.05.05.11";
+export const VERSION = "2025.05.06.0";
 
 // global environment variables :BenchWebEnv
 export const COMMIT = import.meta.env.VITE_COMMIT;
--- bench/sql/schema.py
+++ bench/sql/schema.py
@@ -11,7 +11,7 @@ from .core import (
     Table,
 )
 
-VERSION = "2025.05.05.11"
+VERSION = "2025.05.06.0"
 
 BENCH_TABLE = Table(
     "bench_bench",
