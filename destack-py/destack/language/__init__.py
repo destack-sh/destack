01278372f15e7f57aa3647ea9f9d76b0464ea19d
Florian Cäsar <florian.caesar@pm.me>
2025-06-12 21:28:45 +0200

    chore: update SDK packaging

--- destack-py/destack/language/__init__.py
+++ destack-py/destack/language/__init__.py
@@ -12,7 +12,7 @@ from .finance import *  # noqa: F403
 from .folder import *  # noqa: F403
 from .infra import *  # noqa: F403
 from .intelligence import *  # noqa: F403
-from .interactive import *  # noqa: F403
+from .interaction import *  # noqa: F403
 from .logic import *  # noqa: F403
 from .registry import (
     ENUM_CLASS_BY_TYPE,  # noqa: F401
