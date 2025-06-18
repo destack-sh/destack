a96a2a1668fa5cd1c6f9b6d3f60b985a83fc0c77
Florian Cäsar <florian.caesar@pm.me>
2025-06-18 17:21:59 +0200

    chore(ts): scaffold Typescript language generation

--- destack-ts/src/language/core/builtin/struct.ts
+++ destack-ts/src/language/core/builtin/struct.ts
@@ -1,4 +1,4 @@
-import { AnyStructProto } from "@/proto/wire";
+import { AnyStructProto } from "@/proto";
 import { BuiltinObject } from "./object";
 
 /** A Struct is an ordered collection of Properties. */
@@ -17,12 +17,4 @@ export abstract class StructFrozen extends Struct {
 	readonly _repr: string | null = null;
 	readonly _proto: AnyStructProto | null = null;
 	readonly _value: Record<string, any> | null = null;
-
-	_invalidateFrozenCache(): void {
-		// frozen Structs should be immutable, but sometimes we need to break out of that
-		this._hash = null; // @ts-expect-error
-		this._repr = null; // @ts-expect-error
-		this._proto = null; // @ts-expect-error
-		this._value = null; // @ts-expect-error
-	}
 }
