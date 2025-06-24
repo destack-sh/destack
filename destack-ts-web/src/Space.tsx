d86b9119ed60b01e8dd074d2f00c60674c0435f7
Florian Cäsar <florian.caesar@pm.me>
2025-06-24 09:41:26 +0200

    chore(ts): setup local destack-ts package properly

--- destack-ts-web/src/Space.tsx
+++ destack-ts-web/src/Space.tsx
@@ -4,12 +4,12 @@ const canvas = new Canvas({
   name: "My Canvas",
 });
 
-const lines = canvas.getChildren(NodeType.LINE)
+const lines = canvas.getChildren({ nodeType: NodeType.LINE_SHAPE });
 
 const Space: React.FC = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World!</h1>
     </div>
   );
 };
