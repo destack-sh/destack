7bfa27f4d5b9a567f0b8ffd599b51b94f0b17170
Florian Cäsar <florian.caesar@pm.me>
2025-06-25 11:28:47 +0200

    test(py): update test strategies with unified Properties

--- destack-py-system/desys/store/postgres/query.py
+++ destack-py-system/desys/store/postgres/query.py
@@ -35,7 +35,7 @@ from destack.language.core.common.relation import PropertyReferenceType
 from destack.utils.uuid import UUID
 
 from .core import PostgresContext
-from .map import DESTACK_CUSTOM_FIELD_PREFIX
+from .map import DESTACK_CUSTOM_PROPERTY_PREFIX
 from .wiring import pack_column_flat, unpack_node_row
 
 logger = structlog.get_logger(__name__)
@@ -76,7 +76,7 @@ def _compile_attribute(
     elif attribute.type == PropertyReferenceType.CUSTOM:
         field = attribute.resolve_or_error()
         assert isinstance(field, CustomProperty), f"no field for {attribute!r}"
-        field_name = f"{DESTACK_CUSTOM_FIELD_PREFIX}{str(field.id).replace('-', '')}"
+        field_name = f"{DESTACK_CUSTOM_PROPERTY_PREFIX}{str(field.id).replace('-', '')}"
         if field.scalar_type == ScalarType.NODE_REFERENCE:
             return f"{field_name}_id"
         else:
