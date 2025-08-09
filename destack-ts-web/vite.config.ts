246c261db065fc705915bb7bbe5eadd9b4f8c89b
Florian Cäsar <florian.caesar@pm.me>
2025-08-09 17:11:40 +0200

    chore(ts-web): remove pyodide

--- destack-ts-web/vite.config.ts
+++ destack-ts-web/vite.config.ts
@@ -1,29 +1,13 @@
 // @ts-nocheck
-import { dirname, join } from "node:path";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-const PYODIDE_EXCLUDE = ["!**/*.{md,html}", "!**/*.d.ts", "!**/*.whl", "!**/node_modules"];
-
-export function viteStaticCopyPyodide() {
-  const pyodideDir = dirname(fileURLToPath(import.meta.resolve("pyodide")));
-  return viteStaticCopy({
-    targets: [
-      {
-        src: [join(pyodideDir, "*")].concat(PYODIDE_EXCLUDE),
-        dest: "assets",
-      },
-    ],
-  });
-}
 
 // https://vitejs.dev/config/
 const defaultConfig = defineConfig({
   logLevel: "info",
-  optimizeDeps: { exclude: ["pyodide", "destack"] },
+  optimizeDeps: { exclude: ["destack"] },
   plugins: [
     babel({
       babelConfig: {
@@ -35,7 +19,6 @@ const defaultConfig = defineConfig({
         ],
       },
     }),
-    viteStaticCopyPyodide(),
     tsconfigPaths({
       projects: ["./tsconfig.json", "../destack-ts/tsconfig.json"],
     }),
