ac49a870a97dc5b37c32d1634fd847351b996da8
Florian Cäsar <florian.caesar@pm.me>
2025-08-13 13:35:12 +0200

    feat(language): prepare physics simulation, integrate with presentation/Views

--- destack/presentation/view/layout.py
+++ destack/presentation/view/layout.py
@@ -21,7 +21,7 @@ if TYPE_CHECKING:
     NodeType.LAYOUT_VIEW2D,
     is_abstract=True,
     expected_descendant_types=(NodeType.VIEW2D,),
-    tags=(TagDeclaration(id=160, name="layout", description="The layout of the View."),),
+    tags=(TagDeclaration(id=180, name="layout", description="The layout of the View."),),
 )
 class LayoutView2D(View2D):
     """
@@ -30,52 +30,52 @@ class LayoutView2D(View2D):
 
     # layout
     layout: Optional["Layout"] = declare_property(
-        160,
+        180,
         description="How to layout the children of this View.",
         tag="layout",
     )
     direction: Optional["Direction"] = declare_property(
-        161,
+        181,
         description="The direction in which the children of this View are laid out.",
         tag="layout",
     )
     distribute: Optional["Distribute"] = declare_property(
-        162,
+        182,
         description="How to distribute the children of this View.",
         tag="layout",
     )
     align: Optional["Align"] = declare_property(
-        163,
+        183,
         description="How to align the children of this View.",
         tag="layout",
     )
     gap: Optional["Axis2"] = declare_property(
-        164,
+        184,
         description="The gap between the children of this View.",
         tag="layout",
     )
     padding: Optional["Inset2"] = declare_property(
-        165,
+        185,
         description="The padding around the children of this View.",
         tag="layout",
     )
     grid: Optional["Grid2"] = declare_property(
-        166,
+        186,
         description="The grid in which the children of this View are laid out.",
         tag="layout",
     )
     grid_span: Optional["GridSpan2"] = declare_property(
-        167,
+        187,
         description="The span of this View in the grid.",
         tag="layout",
     )
     aspect_ratio: Optional[Float32] = declare_property(
-        168,
+        188,
         description="The aspect ratio of this View.",
         tag="layout",
     )
     is_wrap: Optional[bool] = declare_property(
-        169,
+        189,
         description="Whether the children of this View are wrapped in the grid.",
         tag="layout",
     )
--- destack/simulation/physics/__init__.py
+++ destack/simulation/physics/__init__.py
@@ -1,6 +1,5 @@
 from .body import Body2D, Body3D
 from .collider import Collider2D, Collider3D
-from .dynamic import DynamicBody2D, DynamicBody3D
 from .joint import (
     FixedJoint2D,
     FixedJoint3D,
@@ -16,34 +15,29 @@ from .joint import (
     WheelJoint2D,
     WheelJoint3D,
 )
-from .kinematic import KinematicBody2D, KinematicBody3D
+from .rigid import RigidBody2D, RigidBody3D
 from .soft import SoftBody2D, SoftBody3D
-from .static import StaticBody2D, StaticBody3D
 
 __all__ = [
     "Body2D",
     "Body3D",
     "Collider2D",
     "Collider3D",
-    "DynamicBody2D",
-    "DynamicBody3D",
     "FixedJoint2D",
     "FixedJoint3D",
     "HingeJoint2D",
     "HingeJoint3D",
     "Joint2D",
     "Joint3D",
-    "KinematicBody2D",
-    "KinematicBody3D",
     "PrismaticJoint2D",
     "PrismaticJoint3D",
+    "RigidBody2D",
+    "RigidBody3D",
     "RopeJoint2D",
     "RopeJoint3D",
     "SoftBody2D",
     "SoftBody3D",
     "SphericalJoint3D",
-    "StaticBody2D",
-    "StaticBody3D",
     "WheelJoint2D",
     "WheelJoint3D",
 ]
