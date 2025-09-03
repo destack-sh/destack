e8a33750e059a3a6d44bc9c32016c1030d5a913a
Florian Cäsar <florian.caesar@pm.me>
2025-09-03 13:29:38 +0200

    feat(language/ast): expand parser

--- language/ast/rust/parse/enum.rs
+++ language/ast/rust/parse/enum.rs
@@ -3,10 +3,29 @@
 use crate::{Enum, NodeId, ParseResult, Parser};
 
 impl<'a> Parser<'a> {
+    /// Eat an enum declaration.
+    ///
+    /// Examples:
+    /// ```
+    /// // anonymous enum (for use as a value)
+    /// enum { Success, Failure }
+    ///
+    /// enum Foo {
+    ///     A // semicolon optional
+    ///     B
+    ///     C
+    /// }
+    ///
+    /// enum(u8) Foo {
+    ///     Baz = 1
+    ///     Qux = 2
+    /// }
+    /// ```
     pub fn eat_enum(&mut self) -> ParseResult<NodeId<Enum>> {
         todo!()
     }
 
+    /// Eat an enum body (without the header or `{` and `}`)
     pub fn eat_enum_body(&mut self) -> ParseResult<NodeId<Enum>> {
         todo!()
     }
