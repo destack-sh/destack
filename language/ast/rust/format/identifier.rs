c92037f644b196bfbbdf6cc2f222a50b75fa66c0
Florian Cäsar <florian.caesar@pm.me>
2025-09-19 22:28:09 +0200

    chore(language): prepare AST/FIR formatting

--- language/ast/rust/format/identifier.rs
+++ language/ast/rust/format/identifier.rs
@@ -1,4 +1,4 @@
-use crate::{DystFormatContext, DystFormatter};
+use crate::{DystFormatContext, DystFormatter, Keyword};
 use dyst_language_fir::format::text;
 use dyst_language_fir::prelude::*;
 use dyst_language_fir::write;
@@ -9,5 +9,12 @@ impl<'ast> Format<DystFormatContext<'ast>> for StringId {
     fn format(&self, f: &mut DystFormatter<'ast, '_>) -> FormatResult<()> {
         let string = f.context().get_string(*self).to_string();
         write!(f, [text(&string)])
+    }
+}
+
+impl<'ast> Format<DystFormatContext<'ast>> for Keyword {
+    #[inline]
+    fn format(&self, f: &mut DystFormatter<'ast, '_>) -> FormatResult<()> {
+        write!(f, [text(self.as_str())])
     }
 }
