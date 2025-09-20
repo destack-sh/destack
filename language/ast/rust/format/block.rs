2e36d4f220801f960a00070efd5492381c8a2d7a
Florian Cäsar <florian.caesar@pm.me>
2025-09-20 16:25:59 +0200

    feat(language): support basic AST formatting

--- language/ast/rust/format/block.rs
+++ language/ast/rust/format/block.rs
@@ -1,7 +1,8 @@
 use dyst_language_fir::format::FormatResult;
 
-use crate::{Block, DystFormatter, FormatNode, NodeId};
+use crate::{Block, Break, Continue, Defer, DystFormatter, FormatNode, Keyword, NodeId, Return};
 use dyst_language_fir::prelude::*;
+use dyst_language_fir::{format_args, write};
 
 impl<'ast> FormatNode<'ast, Block> for Block {
     fn format_node(
@@ -9,6 +10,96 @@ impl<'ast> FormatNode<'ast, Block> for Block {
         _node_id: NodeId<Block>,
         f: &mut DystFormatter<'ast, '_>,
     ) -> FormatResult<()> {
-        todo!()
+        // label
+        if let Some(label) = &self.label {
+            write!(f, [label, token(": ")])?;
+        }
+        // statements
+		if self.statements.is_empty() {
+			write!(f, [token("{ }")])?;
+			return Ok(());
+		}
+        write!(
+            f,
+            [group(&format_args![
+                token("{"),
+                hard_line_break(),
+                block_indent(&format_with(|f| f
+                    .join_with(hard_line_break())
+                    .entries(&self.statements)
+                    .finish())),
+                hard_line_break(),
+                token("}")
+            ])]
+        )?;
+        Ok(())
+    }
+}
+
+impl<'ast> FormatNode<'ast, Break> for Break {
+    fn format_node(
+        &self,
+        _node_id: NodeId<Break>,
+        f: &mut DystFormatter<'ast, '_>,
+    ) -> FormatResult<()> {
+        write!(f, [Keyword::Break])?;
+        // label
+        if let Some(label) = &self.label {
+            write!(f, [token(": "), label])?;
+        }
+        // value
+        if let Some(value) = &self.value {
+            write!(f, [token(" "), value])?;
+        }
+        Ok(())
+    }
+}
+
+impl<'ast> FormatNode<'ast, Continue> for Continue {
+    fn format_node(
+        &self,
+        _node_id: NodeId<Continue>,
+        f: &mut DystFormatter<'ast, '_>,
+    ) -> FormatResult<()> {
+        write!(f, [Keyword::Continue])?;
+        // label
+        if let Some(label) = &self.label {
+            write!(f, [token(": "), label])?;
+        }
+        Ok(())
+    }
+}
+
+impl<'ast> FormatNode<'ast, Return> for Return {
+    fn format_node(
+        &self,
+        _node_id: NodeId<Return>,
+        f: &mut DystFormatter<'ast, '_>,
+    ) -> FormatResult<()> {
+        write!(f, [Keyword::Return])?;
+        // value
+        if let Some(value) = &self.value {
+            write!(f, [token(" "), value])?;
+        }
+        Ok(())
+    }
+}
+
+impl<'ast> FormatNode<'ast, Defer> for Defer {
+    fn format_node(
+        &self,
+        _node_id: NodeId<Defer>,
+        f: &mut DystFormatter<'ast, '_>,
+    ) -> FormatResult<()> {
+        write!(f, [Keyword::Defer])?;
+        match self {
+            Defer::Expression(expression) => {
+                write!(f, [token(" "), expression])?;
+            }
+            Defer::Block(block) => {
+                write!(f, [token(" "), block])?;
+            }
+        }
+        Ok(())
     }
 }
--- language/ast/rust/format/expression.rs
+++ language/ast/rust/format/expression.rs
@@ -1,5 +1,6 @@
 use dyst_language_fir::format::FormatResult;
 use dyst_language_fir::prelude::*;
+use dyst_language_fir::{format_args, write};
 
 use crate::{DystFormatter, Expression, FormatNode, NodeId};
 
@@ -18,14 +19,49 @@ impl<'ast> FormatNode<'ast, Expression> for Expression {
             Expression::Implement(node) => node.format(f),
             Expression::Function(node) => node.format(f),
 
+            Expression::Let(node) => node.format(f),
+            Expression::Block(node) => node.format(f),
+            Expression::If(node) => node.format(f),
+            Expression::While(node) => node.format(f),
+            Expression::For(node) => node.format(f),
+            Expression::Loop(node) => node.format(f),
+            Expression::Break(node) => node.format(f),
+            Expression::Continue(node) => node.format(f),
+            Expression::Defer(node) => node.format(f),
+            Expression::Return(node) => node.format(f),
+            Expression::Try(node) => node.format(f),
+            Expression::Match(node) => node.format(f),
+
             Expression::Path(p) => p.format(f),
             Expression::ScalarLiteral(node) => node.format(f),
             Expression::RangeLiteral(node) => node.format(f),
-            Expression::TupleLiteral(node) => node.format(f),
             Expression::ArrayLiteral(node) => node.format(f),
+            Expression::TupleLiteral(node) => node.format(f),
             Expression::StructLiteral(node) => node.format(f),
 
-            _ => todo!("{self:?}"),
+            Expression::Unary { operator, right } => todo!("unary {operator:?} {right:?}"),
+            Expression::Reference { mutability, right } => {
+                todo!("reference {mutability:?} {right:?}")
+            }
+            Expression::Member { receiver, path } => todo!("member {receiver:?}.{path:?}"),
+            Expression::Index(node) => todo!("index {node:?}"),
+            Expression::Call(node) => node.format(f),
+            Expression::Cast(node) => todo!("cast {node:?}"),
+            Expression::Unwrap(expr) => write!(f, [expr, token("?")]),
+            Expression::UnwrapOrPanic(expr) => write!(f, [expr, token("!")]),
+            Expression::Coalesce(node) => todo!("coalesce {node:?}"),
+            Expression::Binary {
+                left,
+                operator,
+                right,
+            } => todo!("binary {left:?} {operator:?} {right:?}"),
+            Expression::Assign {
+                left,
+                operator,
+                right,
+            } => todo!("assign {left:?} {operator:?} {right:?}"),
+
+            Expression::Error => todo!("error"),
         }
     }
 }
