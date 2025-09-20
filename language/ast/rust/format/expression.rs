d5e71ea8a5a091a357ee75271e9d39ad814487fc
Florian Cäsar <florian.caesar@pm.me>
2025-09-20 16:57:01 +0200

    feat(language): support basic AST formatting

--- language/ast/rust/format/expression.rs
+++ language/ast/rust/format/expression.rs
@@ -1,8 +1,11 @@
 use dyst_language_fir::format::FormatResult;
 use dyst_language_fir::prelude::*;
-use dyst_language_fir::{format_args, write};
+use dyst_language_fir::write;
 
-use crate::{DystFormatter, Expression, FormatNode, NodeId};
+use crate::{
+    AssignOperator, BinaryOperator, DystFormatContext, DystFormatter, Expression, FormatNode,
+    Mutability, NodeId, UnaryOperator,
+};
 
 impl<'ast> FormatNode<'ast, Expression> for Expression {
     fn format_node(
@@ -39,29 +42,136 @@ impl<'ast> FormatNode<'ast, Expression> for Expression {
             Expression::TupleLiteral(node) => node.format(f),
             Expression::StructLiteral(node) => node.format(f),
 
-            Expression::Unary { operator, right } => todo!("unary {operator:?} {right:?}"),
-            Expression::Reference { mutability, right } => {
-                todo!("reference {mutability:?} {right:?}")
+            Expression::Unary { operator, right } => {
+                write!(f, [operator, right])
             }
-            Expression::Member { receiver, path } => todo!("member {receiver:?}.{path:?}"),
-            Expression::Index(node) => todo!("index {node:?}"),
+            Expression::Reference { mutability, right } => match mutability {
+                Mutability::Mutable => write!(f, [token("&var "), right]),
+                Mutability::Immutable => write!(f, [token("&"), right]),
+            },
+            Expression::Member { receiver, path } => write!(f, [receiver, token("."), path]),
+            Expression::Index(node) => node.format(f),
             Expression::Call(node) => node.format(f),
-            Expression::Cast(node) => todo!("cast {node:?}"),
+            Expression::Cast(node) => {
+                let node = f.context().get_node(*node).clone();
+                write!(f, [node.receiver, space(), token("as"), space(), node.r#type])
+            }
             Expression::Unwrap(expr) => write!(f, [expr, token("?")]),
             Expression::UnwrapOrPanic(expr) => write!(f, [expr, token("!")]),
-            Expression::Coalesce(node) => todo!("coalesce {node:?}"),
+            Expression::Coalesce(node) => {
+                let node = f.context().get_node(*node).clone();
+                write!(
+                    f,
+                    [node.receiver, space(), token("??"), space(), node.default]
+                )
+            }
             Expression::Binary {
                 left,
                 operator,
                 right,
-            } => todo!("binary {left:?} {operator:?} {right:?}"),
+            } => write!(f, [left, space(), operator, space(), right]),
             Expression::Assign {
                 left,
                 operator,
                 right,
-            } => todo!("assign {left:?} {operator:?} {right:?}"),
+            } => write!(f, [left, space(), operator, space(), right]),
 
-            Expression::Error => todo!("error"),
+            Expression::Error => panic!("invalid expression: {self:?}"),
         }
+    }
+}
+
+impl<'ast> Format<DystFormatContext<'ast>> for UnaryOperator {
+    fn format(&self, f: &mut DystFormatter<'ast, '_>) -> FormatResult<()> {
+        let token = match self {
+            UnaryOperator::Not => token("!"),
+            UnaryOperator::Negate => token("-"),
+            UnaryOperator::WrappingNegate => token("-%"),
+            UnaryOperator::BitwiseNot => token("~"),
+            UnaryOperator::Dereference => token("*"),
+        };
+        write!(f, [token])
+    }
+}
+
+impl<'ast> Format<DystFormatContext<'ast>> for BinaryOperator {
+    fn format(&self, f: &mut DystFormatter<'ast, '_>) -> FormatResult<()> {
+        let token = match self {
+            // multiplication
+            BinaryOperator::Multiply => token("*"),
+            BinaryOperator::WrappingMultiply => token("*%"),
+            BinaryOperator::SaturatingMultiply => token("*|"),
+            BinaryOperator::Divide => token("/"),
+            BinaryOperator::Remainder => token("%"),
+
+            // addition
+            BinaryOperator::Add => token("+"),
+            BinaryOperator::WrappingAdd => token("+%"),
+            BinaryOperator::SaturatingAdd => token("+|"),
+            BinaryOperator::Subtract => token("-"),
+            BinaryOperator::WrappingSubtract => token("-%"),
+            BinaryOperator::SaturatingSubtract => token("-|"),
+
+            // shift
+            BinaryOperator::ShiftLeft => token("<<"),
+            BinaryOperator::SaturatingShiftLeft => token("<<|"),
+            BinaryOperator::ShiftRight => token(">>"),
+
+            // bitwise
+            BinaryOperator::BitwiseAnd => token("&"),
+            BinaryOperator::BitwiseXor => token("^"),
+            BinaryOperator::BitwiseOr => token("|"),
+
+            // comparison
+            BinaryOperator::Equal => token("=="),
+            BinaryOperator::NotEqual => token("!="),
+            BinaryOperator::LessThan => token("<"),
+            BinaryOperator::LessThanOrEqual => token("<="),
+            BinaryOperator::GreaterThan => token(">"),
+            BinaryOperator::GreaterThanOrEqual => token(">="),
+
+            // logical
+            BinaryOperator::And => token("&&"),
+            BinaryOperator::Or => token("||"),
+        };
+        write!(f, [token])
+    }
+}
+
+impl<'ast> Format<DystFormatContext<'ast>> for AssignOperator {
+    fn format(&self, f: &mut DystFormatter<'ast, '_>) -> FormatResult<()> {
+        let token = token(match self {
+            AssignOperator::Assign => "=",
+
+            // addition
+            AssignOperator::AddAssign => "+=",
+            AssignOperator::WrappingAddAssign => "+%=",
+            AssignOperator::SaturatingAddAssign => "+|=",
+            AssignOperator::SubtractAssign => "-=",
+            AssignOperator::WrappingSubtractAssign => "-%=",
+            AssignOperator::SaturatingSubtractAssign => "-|=",
+
+            // multiplication
+            AssignOperator::MultiplyAssign => "*=",
+            AssignOperator::WrappingMultiplyAssign => "*%=",
+            AssignOperator::SaturatingMultiplyAssign => "*|=",
+            AssignOperator::DivideAssign => "/=",
+            AssignOperator::RemainderAssign => "%=",
+
+            // shift
+            AssignOperator::ShiftLeftAssign => "<<=",
+            AssignOperator::SaturatingShiftLeftAssign => "<<|=",
+            AssignOperator::ShiftRightAssign => ">>=",
+
+            // bitwise
+            AssignOperator::BitwiseAndAssign => "&=",
+            AssignOperator::BitwiseOrAssign => "|=",
+            AssignOperator::BitwiseXorAssign => "^=",
+
+            // logical
+            AssignOperator::AndAssign => "&&=",
+            AssignOperator::OrAssign => "||=",
+        });
+        write!(f, [token])
     }
 }
--- language/ast/rust/format/struct.rs
+++ language/ast/rust/format/struct.rs
@@ -1,7 +1,9 @@
 use dyst_language_fir::format::FormatResult;
+use dyst_language_fir::format_args;
 
-use crate::{DystFormatter, FormatNode, NodeId, Struct};
+use crate::{DystFormatter, FormatNode, Keyword, NodeId, Struct, StructField};
 use dyst_language_fir::prelude::*;
+use dyst_language_fir::write;
 
 impl<'ast> FormatNode<'ast, Struct> for Struct {
     fn format_node(
@@ -9,6 +11,148 @@ impl<'ast> FormatNode<'ast, Struct> for Struct {
         _node_id: NodeId<Struct>,
         f: &mut DystFormatter<'ast, '_>,
     ) -> FormatResult<()> {
-        todo!()
+        // header
+        write!(f, [Keyword::Struct])?;
+        if let Some(representation_type) = self.representation_type {
+            write!(f, [token("("), representation_type, token(")"), space()])?;
+        } else {
+            write!(f, [space()])?;
+        }
+        if let Some(name) = self.name {
+            write!(f, [name, space()])?;
+        }
+        if self.fields.is_empty() && self.statements.is_empty() {
+            write!(f, [token("{ }")])?;
+            return Ok(());
+        }
+        write!(f, [token("{"), hard_line_break()])?;
+        // body
+        // fields
+        write!(
+            f,
+            [group(&format_args![block_indent(&format_with(|f| f
+                .join_with(hard_line_break())
+                .entries(&self.fields)
+                .finish())),])]
+        )?;
+        // blank line
+        if !self.fields.is_empty() && !self.statements.is_empty() {
+            write!(f, [hard_line_break()])?;
+        }
+        // statements
+        write!(
+            f,
+            [group(&format_args![block_indent(&format_with(|f| f
+                .join_with(hard_line_break())
+                .entries(&self.statements)
+                .finish())),])]
+        )?;
+        write!(f, [hard_line_break(), token("}")])?;
+        Ok(())
+    }
+}
+
+impl<'ast> FormatNode<'ast, StructField> for StructField {
+    fn format_node(
+        &self,
+        _node_id: NodeId<StructField>,
+        f: &mut DystFormatter<'ast, '_>,
+    ) -> FormatResult<()> {
+        if let Some(name) = self.name {
+            write!(f, [name, token(": ")])?;
+        }
+        write!(f, [self.r#type])?;
+        if let Some(default) = self.default {
+            write!(f, [token(" = "), default])?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::format::tests::TestFormatter;
+    use crate::{DystFormatOptions, assert_format};
+
+    #[test]
+    fn test_format_struct_empty() {
+        assert_format!(
+            "struct { }",
+            "struct { }",
+            |p| p.eat_struct(None),
+            DystFormatOptions::default()
+        );
+    }
+
+    #[test]
+    fn test_format_struct_with_fields() {
+        assert_format!(
+            "struct { a: int32, b: boolean }",
+            "struct {\n\ta: int32\n\tb: boolean\n}",
+            |p| p.eat_struct(None),
+            DystFormatOptions::default_tab()
+        );
+    }
+
+    #[test]
+    fn test_format_struct_with_name() {
+        assert_format!(
+            "struct Foo { a: int32 }",
+            "struct Foo {\n\ta: int32\n}",
+            |p| p.eat_struct(None),
+            DystFormatOptions::default_tab()
+        );
+    }
+
+    #[test]
+    fn test_format_struct_with_representation_type() {
+        assert_format!(
+            "struct(uint64) Foo { a: int32 }",
+            "struct(uint64) Foo {\n\ta: int32\n}",
+            |p| p.eat_struct(None),
+            DystFormatOptions::default_tab()
+        );
+    }
+
+    #[test]
+    fn test_format_struct_with_tuple_fields() {
+        assert_format!(
+            "struct Foo(int32, boolean) { }",
+            "struct Foo(int32, boolean) { }",
+            |p| p.eat_struct(None),
+            DystFormatOptions::default()
+        );
+    }
+
+    #[test]
+    fn test_format_struct_with_fields_and_defaults() {
+        assert_format!(
+            "struct { a: int32 = 42, b: boolean }",
+            "struct {\n\ta: int32 = 42\n\tb: boolean\n}",
+            |p| p.eat_struct(None),
+            DystFormatOptions::default_tab()
+        );
+    }
+
+    #[test]
+    fn test_format_struct_with_statements() {
+        assert_format!(
+            r"struct { let X = 1 }",
+            r"struct {
+	let X = 1
+}",
+            |p| p.eat_struct(None),
+            DystFormatOptions::default_tab()
+        );
+    }
+
+    #[test]
+    fn test_format_struct_with_fields_and_statements() {
+        assert_format!(
+            "struct { a: int32, let X = 1 }",
+            "struct {\n\ta: int32\n\n\tlet X = 1\n}",
+            |p| p.eat_struct(None),
+            DystFormatOptions::default_tab()
+        );
     }
 }
