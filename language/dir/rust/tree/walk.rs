886bc8db9b7bb3c089c8134efc91ded5cc88d813
Florian Cäsar <florian.caesar@pm.me>
2025-10-04 20:52:20 +0200

    chore(language): prepare DIR

--- language/dir/rust/tree/walk.rs
+++ language/dir/rust/tree/walk.rs
@@ -1,6 +1,8 @@
-use dyst_ast::NodeId;
-
-use crate::{NodeTree, NodeType, NodeVisitor};
+use crate::{
+    Annotation, Argument, Block, Definition, Expression, MatchCase, NodeId, NodeTree, NodeType,
+    NodeVisitor, Parameter, Pattern, PatternField, Type, UseItem, Variant, VariantField,
+    WithAssertion, WithDeclaration,
+};
 
 // ----------------------------------------------------------------------------
 // Traversal functions
@@ -91,4 +93,177 @@ pub fn walk_any(visitor: &mut dyn NodeVisitor, tree: &NodeTree, node_type: NodeT
             walk_annotation(visitor, tree, NodeId::new(node_id), annotation);
         }
     }
+}
+
+// ----------------------------------------------------------------------------
+// Groupings
+// ----------------------------------------------------------------------------
+
+/// Walk the Expression.
+pub fn walk_expression(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<Expression>,
+    expression: &Expression,
+) {
+    todo!()
+}
+
+/// Walk the Block.
+pub fn walk_block(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<Block>,
+    block: &Block,
+) {
+    todo!()
+}
+
+// ----------------------------------------------------------------------------
+// Declarations
+// ----------------------------------------------------------------------------
+
+/// Walk the Definition.
+pub fn walk_definition(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<Definition>,
+    definition: &Definition,
+) {
+    todo!()
+}
+
+// ----------------------------------------------------------------------------
+// Types
+// ----------------------------------------------------------------------------
+
+/// Walk the Type.
+pub fn walk_type(visitor: &mut dyn NodeVisitor, tree: &NodeTree, id: NodeId<Type>, ty: &Type) {
+    todo!()
+}
+
+/// Walk the Variant.
+pub fn walk_variant(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<Variant>,
+    variant: &Variant,
+) {
+    todo!()
+}
+
+/// Walk the VariantField.
+pub fn walk_variant_field(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<VariantField>,
+    variant_field: &VariantField,
+) {
+    todo!()
+}
+
+// ----------------------------------------------------------------------------
+// Context
+// ----------------------------------------------------------------------------
+
+/// Walk the WithDeclaration.
+pub fn walk_with_declaration(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<WithDeclaration>,
+    with_declaration: &WithDeclaration,
+) {
+    todo!()
+}
+
+/// Walk the WithAssertion.
+pub fn walk_with_assertion(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<WithAssertion>,
+    with_assertion: &WithAssertion,
+) {
+    todo!()
+}
+
+/// Walk the UseItem.
+pub fn walk_use_item(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<UseItem>,
+    use_item: &UseItem,
+) {
+    todo!()
+}
+
+// ----------------------------------------------------------------------------
+// Bindings
+// ----------------------------------------------------------------------------
+
+/// Walk the Parameter.
+pub fn walk_parameter(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<Parameter>,
+    parameter: &Parameter,
+) {
+    todo!()
+}
+
+/// Walk the Argument.
+pub fn walk_argument(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<Argument>,
+    argument: &Argument,
+) {
+    todo!()
+}
+
+// ----------------------------------------------------------------------------
+// Matching
+// ----------------------------------------------------------------------------
+
+/// Walk the Pattern.
+pub fn walk_pattern(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<Pattern>,
+    pattern: &Pattern,
+) {
+    todo!()
+}
+
+/// Walk the PatternField.
+pub fn walk_pattern_field(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<PatternField>,
+    pattern_field: &PatternField,
+) {
+    todo!()
+}
+
+/// Walk the MatchCase.
+pub fn walk_match_case(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<MatchCase>,
+    match_case: &MatchCase,
+) {
+    todo!()
+}
+
+// ----------------------------------------------------------------------------
+// Annotations
+// ----------------------------------------------------------------------------
+
+/// Walk the Annotation.
+pub fn walk_annotation(
+    visitor: &mut dyn NodeVisitor,
+    tree: &NodeTree,
+    id: NodeId<Annotation>,
+    annotation: &Annotation,
+) {
+    todo!()
 }
