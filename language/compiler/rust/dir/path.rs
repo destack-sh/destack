286ea00466497a3c085c272f5de8fb9cab983aee
Florian Cäsar <florian.caesar@pm.me>
2025-10-09 14:16:45 +0200

    chore(language): prepare DIR compiler

---
+++ language/compiler/rust/dir/path.rs
@@ -0,0 +1,17 @@
+use crate::Compiler;
+use dyst_ast as ast;
+use dyst_dir::{NodeId, Path};
+use dyst_source::SourceId;
+
+impl<'a> Compiler<'a> {
+    /// Lower a path to a DIR path.
+    pub fn lower_path(
+        &mut self,
+        source_id: SourceId,
+        ast: &ast::NodeTree,
+        path_id: ast::PathId,
+    ) -> Path {
+		// let path = document = ...
+        todo!("Compiler::lower_path")
+    }
+}
