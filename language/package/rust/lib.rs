0f7ab359111d381af7405052d9576fcf3b79cea5
Florian Cäsar <florian.caesar@pm.me>
2025-10-13 21:19:32 +0200

    chore(language): add LanguageMode/LanguageOptions

--- language/package/rust/lib.rs
+++ language/package/rust/lib.rs
@@ -1,9 +1,14 @@
+#![feature(default_field_values)]
+#![feature(if_let_guard)]
+
 mod dependency;
 mod file;
+mod options;
 mod package;
 mod workspace;
 
 pub use dependency::*;
 pub use file::*;
+pub use options::*;
 pub use package::*;
 pub use workspace::*;
