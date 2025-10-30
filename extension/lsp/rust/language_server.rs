d293779ef93607dcda2980794dde88917e0d4262
Florian Cäsar <florian.caesar@pm.me>
2025-10-30 22:13:09 +0100

    chore(extension/vscode): prepare textmate grammar

--- extension/lsp/rust/language_server.rs
+++ extension/lsp/rust/language_server.rs
@@ -46,16 +46,17 @@ impl LanguageServer for DestackLanguageServer {
             text_document_sync: Some(lsp::TextDocumentSyncCapability::Kind(
                 lsp::TextDocumentSyncKind::FULL,
             )),
-            semantic_tokens_provider: Some(
-                lsp::SemanticTokensServerCapabilities::SemanticTokensOptions(
-                    lsp::SemanticTokensOptions {
-                        legend: semantic::legend(),
-                        range: Some(true),
-                        full: Some(lsp::SemanticTokensFullOptions::Bool(true)),
-                        work_done_progress_options: lsp::WorkDoneProgressOptions::default(),
-                    },
-                ),
-            ),
+            // nocheckin
+            // semantic_tokens_provider: Some(
+            //     lsp::SemanticTokensServerCapabilities::SemanticTokensOptions(
+            //         lsp::SemanticTokensOptions {
+            //             legend: semantic::legend(),
+            //             range: Some(true),
+            //             full: Some(lsp::SemanticTokensFullOptions::Bool(true)),
+            //             work_done_progress_options: lsp::WorkDoneProgressOptions::default(),
+            //         },
+            //     ),
+            // ),
             document_formatting_provider: Some(lsp::OneOf::Left(true)),
             workspace: Some(lsp::WorkspaceServerCapabilities {
                 workspace_folders: Some(lsp::WorkspaceFoldersServerCapabilities {
