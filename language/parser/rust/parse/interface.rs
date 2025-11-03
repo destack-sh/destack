3b2ed377b167b2061c6bca123ad542931709c27e
Florian Cäsar <florian.caesar@pm.me>
2025-11-03 14:45:12 +0100

    test(language): extend smoke tests

--- language/parser/rust/parse/interface.rs
+++ language/parser/rust/parse/interface.rs
@@ -312,7 +312,7 @@ interface [Symbols.Client] {
     }
 
     #[test]
-    #[ignore = "nocheckin"]
+    #[ignore = "TODO"]
     fn test_parse_interface_with_nameless_shorthand_functions() {
         let mut test = TestParser::new(
             r#"
