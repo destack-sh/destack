25c711159311d0c52c8591a7f1816b3d48e8c93b
Florian Cäsar <florian.caesar@pm.me>
2025-11-05 21:56:34 +0100

    refactor(library): turn library into proper(ish) packages

--- client/README.md
+++
@@ -1,3 +0,0 @@
-# Destack Clients: Libraries in other Languages
-
-Work with Destack outside of our builtin Rust/Destack (.rs/ds) bindings.
--- client/destack_ts/typescript/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "@destack/cli";
-export * from "@destack/graph";
-export * from "@destack/language";
-export * from "@destack/utils";
