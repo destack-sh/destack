dfc2186864213a312364551513d3d82b5ae63243
Florian Cäsar <florian.caesar@pm.me>
2025-11-12 15:38:20 +0100

    chore(demo): add more test projects

--- demo/test-bun-react/src/APITester.tsx
+++ demo/test-bun-react/src/APITester.tsx
@@ -1,4 +1,4 @@
-import { useRef, type FormEvent } from "react";
+import { type FormEvent, useRef } from "react";
 
 export function APITester() {
     const responseInputRef = useRef<HTMLTextAreaElement>(null);
@@ -29,7 +29,7 @@ export function APITester() {
             >
                 <select
                     name="method"
-                    className="bg-[#fbf0df] text-[#1a1a1a] py-1.5 px-3 rounded-lg font-bold text-sm min-w-[0px] appearance-none cursor-pointer hover:bg-[#f3d5a3] transition-colors duration-100"
+                    className="bg-[#fbf0df] text-[#1a1a1a] py-1.5 px-3 rounded-lg font-bold text-sm min-w-0 appearance-none cursor-pointer hover:bg-[#f3d5a3] transition-colors duration-100"
                 >
                     <option value="GET" className="py-1">
                         GET
