c8820df94fd8ca1ede965127bd89a62c3e82f302
Florian Cäsar <florian.caesar@pm.me>
2025-11-28 21:20:20 +0100

    refactor(*): reorganize along language, library, platform lines

--- internal/README.md
+++
@@ -1,3 +0,0 @@
-# Destack Internal: Development and Deployment
-
-Stuff we don't intend to expose in any way.
--- language/compiler/src/bind/warning.rs
+++ language/compiler/src/bind/warning.rs
@@ -1,6 +1,6 @@
 use dyst_dir::{GlobalNodeIdAny, Program};
 
-use crate::{TaskWarning, Phase};
+use crate::{Phase, TaskWarning};
 
 /// Warning when binding something.
 #[derive(Debug, Clone, PartialEq)]
