5311d88332f80a88e4557cfb215abee7442ad56a
Florian Cäsar <florian.caesar@pm.me>
2025-11-29 12:18:39 +0100

    chore(language): scaffold workspace, reorganize dependencies

---
+++ language/workspace/src/lib.rs
@@ -0,0 +1,8 @@
+#![feature(default_field_values)]
+#![feature(if_let_guard)]
+#![feature(str_as_str)]
+#![feature(thread_id_value)]
+
+pub mod workspace;
+
+pub use workspace::*;
---
+++ platform/daemon/src/daemon/daemon.rs
@@ -0,0 +1,4 @@
+#[derive(Debug)]
+pub struct Daemon {}
+
+impl Daemon {}
