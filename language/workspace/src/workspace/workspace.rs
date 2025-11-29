ebb6435418c8c089852f8ce66f736d91651fd571
Florian Cäsar <florian.caesar@pm.me>
2025-11-29 10:59:31 +0100

    chore(*): remove deprecated language/workspace

--- language/workspace/src/workspace/workspace.rs
+++
@@ -1,8 +0,0 @@
-use destack_source::FileRegistry;
-
-/// Workspace for interacting with the language.
-#[derive(Debug)]
-pub struct Workspace {
-    /// The files in the workspace.
-    pub files: FileRegistry,
-}
