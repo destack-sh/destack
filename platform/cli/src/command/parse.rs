c18eda886498a95b26563a65ecf715f15fcf8760
Florian Cäsar <florian.caesar@pm.me>
2025-12-05 17:53:01 +0100

    fix(language): parse blank and newline annotations consistently

--- platform/cli/src/command/parse.rs
+++ platform/cli/src/command/parse.rs
@@ -72,6 +72,7 @@ pub fn run(args: &ParseArgs) -> i32 {
     // parse as implicit module
     let mut parser = Parser::lex_file(file.clone(), program.language);
     let expressions = parser.parse();
+    program.diagnostics.merge_from(&parser.diagnostics);
 
     // dump output
     if !silent {
