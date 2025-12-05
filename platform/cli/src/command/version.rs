2b21332aa18def4ad526a397c9c72f8f3547d488
Florian Cäsar <florian.caesar@pm.me>
2025-12-05 19:22:56 +0100

    chore(language): extend dsconfig targets

--- platform/cli/src/command/version.rs
+++ platform/cli/src/command/version.rs
@@ -14,7 +14,7 @@ const FILE_GLOBS_TO_UPDATE: &[&str] = &[
     "*/*/package.json",
     "*/*/*/package.json",
 ];
-const FILE_GLOBS_TO_IGNORE: &[&str] = &["language/resolver/fixtures/"];
+const FILE_GLOBS_TO_IGNORE: &[&str] = &["language/test/fixtures/"];
 
 #[derive(Subcommand, Clone, Debug)]
 pub enum VersionCommands {
