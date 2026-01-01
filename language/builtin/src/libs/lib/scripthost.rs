c76d7b96b2687c935c00ef79e6f4671ab18d4e56
Florian Cäsar <florian.caesar@pm.me>
2026-01-02 00:24:43 +0100

    chore(language/builtin): update libs

--- language/builtin/src/libs/lib/scripthost.rs
+++ language/builtin/src/libs/lib/scripthost.rs
@@ -4,7 +4,7 @@ const LIB_SCRIPTHOST_D_DS: BuiltinLibSource = BuiltinLibSource::new(
     "lib",
     "scripthost",
     "index.d.ts",
-    include_str!(concat!("../../../../lib/scripthost/index.d.ts")),
+    include_str!(concat!("../../../lib/scripthost/index.d.ts")),
 );
 
 pub const LIB_SCRIPTHOST: BuiltinLib =
