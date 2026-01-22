d38c788b156368f71cfdc3836074c2f2c7f8b8cd
Florian Cäsar <florian.caesar@pm.me>
2026-01-22 19:52:53 +0100

    feat(language/source): add DiagnosticStore

--- language/source/src/diagnostic/mod.rs
+++ language/source/src/diagnostic/mod.rs
@@ -2,10 +2,12 @@ mod annotate;
 mod collector;
 mod diagnostic;
 mod print;
+mod store;
 mod suggestion;
 
 pub use annotate::*;
 pub use collector::*;
 pub use diagnostic::*;
 pub use print::*;
+pub use store::*;
 pub use suggestion::*;
