7591441ce2df2e00c1d6699ea19b9c75137db2da
Florian Cäsar <florian.caesar@pm.me>
2026-02-05 00:45:09 +0100

    chore(platform/lsp): support workspace file rename

--- platform/lsp/src/server/mod.rs
+++ platform/lsp/src/server/mod.rs
@@ -1,8 +1,9 @@
 #![allow(clippy::module_inception)]
 
 pub(crate) mod daemon;
-pub(crate) mod helpers;
+pub(crate) mod file;
 pub(crate) mod progress;
 mod server;
+pub(crate) mod token;
 
 pub use server::DestackLanguageServer;
