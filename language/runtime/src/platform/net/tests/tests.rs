f032fc1153c85cfd6d935d9d285d0ef79067af0b
Florian Cäsar <florian.caesar@pm.me>
2026-03-05 17:36:16 +0100

    fix(language/runtime): update tests

--- language/runtime/src/platform/net/tests/tests.rs
+++ language/runtime/src/platform/net/tests/tests.rs
@@ -592,7 +592,7 @@ fn socket_address_from_raw(
 
 #[cfg(unix)]
 fn socket_address_native_from_host_port(
-    binding: &BindingCallContext,
+    _binding: &BindingCallContext,
     host: &str,
     port: u16,
     family: SocketFamily,
