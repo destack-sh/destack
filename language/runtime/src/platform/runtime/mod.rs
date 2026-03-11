ec47f1e86c3a9712be33af2e59df1e538da2277f
Florian Cäsar <florian.caesar@pm.me>
2026-03-11 09:39:10 +0100

    feat(language/runtime): wire up platform.runtime bindings

--- language/runtime/src/platform/runtime/mod.rs
+++ language/runtime/src/platform/runtime/mod.rs
@@ -1,5 +1,5 @@
 #[path = "abi.generated.rs"]
-mod abi_generated;
+pub(crate) mod abi_generated;
 #[path = "bindings.generated.rs"]
 mod bindings_generated;
 
@@ -7,8 +7,16 @@ mod bindings_generated;
 pub use abi_generated::*;
 #[allow(unused_imports, unreachable_pub)]
 pub use bindings_generated::*;
+mod binding;
+mod descriptor;
+mod handle;
 pub mod native;
-pub(crate) mod runtime;
-#[cfg(test)]
-mod tests;
+mod request;
+mod view;
 pub mod vm;
+
+pub(crate) use binding::*;
+pub(crate) use descriptor::*;
+pub(crate) use handle::*;
+pub(crate) use request::*;
+pub(crate) use view::*;
