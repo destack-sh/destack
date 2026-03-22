51886f1be5b9cae387592ea7883d72eafb36a773
Florian Cäsar <florian.caesar@pm.me>
2026-03-22 11:26:45 +0100

    test(language): clean up regression suite entrypoint

--- language/test/src/regression/main.rs
+++ language/test/src/regression/main.rs
@@ -2,11 +2,11 @@ use std::process::ExitCode;
 
 use clap::Parser;
 
-use destack_test::harness::{Runner, TestOptions};
+use destack_test::core::{RunOptions, Runner};
 use destack_test::regression::RegressionSuite;
 
 /// Run Destack regression tests.
 fn main() -> ExitCode {
-    let options = TestOptions::parse();
-    Runner::run_suite(&RegressionSuite, &options)
+    let options = RunOptions::parse();
+    Runner::run_suite(RegressionSuite, &options)
 }
