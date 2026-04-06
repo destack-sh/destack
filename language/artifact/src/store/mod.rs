fd6aa97957cbab1b9173215313f3f7d5d042838c
Florian Cäsar <florian.caesar@pm.me>
2026-04-06 21:47:02 +0200

    feat(language/artifact): add scoped artifact pin sets

--- language/artifact/src/store/mod.rs
+++ language/artifact/src/store/mod.rs
@@ -3,6 +3,7 @@ mod dependency;
 mod family;
 mod image;
 mod key;
+mod pin;
 mod store;
 
 pub use constants::*;
@@ -10,4 +11,5 @@ pub use dependency::*;
 pub use family::*;
 pub use image::*;
 pub use key::*;
+pub use pin::*;
 pub use store::*;
