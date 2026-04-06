dcdd972f765ebe7207ceaa9106d58034ee23035d
Florian Cäsar <florian.caesar@pm.me>
2026-04-06 10:27:38 +0200

    refactor(language/source): generalize image adaptation support

--- language/source/src/lib.rs
+++ language/source/src/lib.rs
@@ -1,12 +1,19 @@
 #![feature(default_field_values)]
 #![feature(if_let_guard)]
 
+#[allow(unused_extern_crates)]
+extern crate self as destack_source;
+
 mod diagnostic;
 mod edit;
 mod file;
+mod image;
 mod tree;
 
+pub use destack_core::StringId;
+pub use destack_source_macros::AdaptImage;
 pub use diagnostic::*;
 pub use edit::*;
 pub use file::*;
+pub use image::*;
 pub use tree::*;
