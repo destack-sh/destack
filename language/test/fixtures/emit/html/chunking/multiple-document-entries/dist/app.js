77e571085cea93d488c6eccb4e595b4d88634bdb
Florian Cäsar <florian.caesar@pm.me>
2026-04-08 09:20:44 +0200

    test(language/compiler/emit): update emit fixtures

--- language/test/fixtures/emit/html/chunking/multiple-document-entries/dist/app.js
+++ language/test/fixtures/emit/html/chunking/multiple-document-entries/dist/app.js
@@ -1,18 +1,9 @@
-const __destack_resource_d90f1d3d = "./shell.css";
-if(typeof document !== "undefined") {
-    const __destack_stylesheet_link_d90f1d3d = document.createElement("link");
-    __destack_stylesheet_link_d90f1d3d.rel="stylesheet";
-    __destack_stylesheet_link_d90f1d3d.href=__destack_resource_d90f1d3d;
-    document.head.appendChild(__destack_stylesheet_link_d90f1d3d);
-}
-export { __destack_resource_d90f1d3d as default };
-
 import {
     getNavigationLabel,
     getPageDescription,
     getPageHeading,
     getPageTitle,
-} from "./page-metadata.js";
+} from "./page-metadata-9e59aaf8.js";
 export const appPageState = {
     pageTitle: getPageTitle("github"),
     pageHeading: getPageHeading("github"),
--- language/test/fixtures/emit/html/style/module-script-stylesheet-imports/dist/site.js
+++ language/test/fixtures/emit/html/style/module-script-stylesheet-imports/dist/site.js
@@ -1,18 +1,2 @@
-const _default = "./panel.css";
-if(typeof document !== "undefined") {
-    const __destack_stylesheet_link_626ca571 = document.createElement("link");
-    __destack_stylesheet_link_626ca571.rel="stylesheet";
-    __destack_stylesheet_link_626ca571.href=_default;
-    document.head.appendChild(__destack_stylesheet_link_626ca571);
-}
-
-const _default2 = "./site.css";
-if(typeof document !== "undefined") {
-    const __destack_stylesheet_link_8f3497d8 = document.createElement("link");
-    __destack_stylesheet_link_8f3497d8.rel="stylesheet";
-    __destack_stylesheet_link_8f3497d8.href=_default2;
-    document.head.appendChild(__destack_stylesheet_link_8f3497d8);
-}
-
 export const pageState = "ready";
 //# sourceMappingURL=./site.js.map
