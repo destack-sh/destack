80700cb5c4467c3b0ceecad232801b71fb5ac0db
Florian Cäsar <florian.caesar@pm.me>
2026-04-20 09:47:39 +0200

    feat(language/ast): allow omitted type member field annotations

--- language/ast/src/tree/type.rs
+++ language/ast/src/tree/type.rs
@@ -14,7 +14,7 @@ pub enum TypeMember {
         is_optional: bool,
         is_readonly: bool,
         key: Key,
-        declared_type: LocalNodeId<TypeExpression>,
+        declared_type: Option<LocalNodeId<TypeExpression>>,
     },
     /// Named method.
     Method {
