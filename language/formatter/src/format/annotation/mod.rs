35f63f7bd03b8cd9a4899b8a25eba3da7863a215
Florian Cäsar <florian.caesar@pm.me>
2026-04-20 21:24:48 +0200

    feat(language/formatter): improve FormatTrailingComments / FormatDanglingComments

--- language/formatter/src/format/annotation/mod.rs
+++ language/formatter/src/format/annotation/mod.rs
@@ -4,13 +4,12 @@ mod trivia;
 
 pub(crate) use self::sequence::{
     block_infix_annotations, decorator_prefix_annotations, infix_or_postfix_annotations,
-    infix_or_postfix_annotations_without_line_suffix_boundary, line_suffix_boundary_annotations,
-    postfix_annotations, postfix_annotations_without_line_suffix_boundary, prefix_annotations,
-    prefix_annotations_after_offset, prefix_annotations_without_decorators,
-    raw_prefix_comment_nodes, write_annotation_sequence, write_inline_prefix_annotations,
+    postfix_annotations, prefix_annotations, prefix_annotations_after_offset,
+    prefix_annotations_without_comments, prefix_annotations_without_decorators,
+    prefix_comment_nodes, write_annotation_sequence, write_inline_prefix_annotations,
 };
 pub(crate) use self::trivia::{
-    format_raw_comment, format_trailing_comment_slice, format_trailing_comments,
-    format_trailing_comments_before_boundary, write_raw_comment_slice, write_raw_leading_comments,
-    write_raw_trailing_comments_without_parent_expansion,
+    DanglingIndentMode, FormatDanglingComments, FormatLeadingComments, FormatTrailingComments,
+    format_comment, format_dangling_comments, format_leading_comments, format_trailing_comments,
+    write_comment_slice,
 };
