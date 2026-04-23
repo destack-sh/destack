765464f43318778563cce19efa572237daf82f54
Florian Cäsar <florian.caesar@pm.me>
2026-04-23 17:44:05 +0200

    refactor(languaeg/test): retire mirbench (yay)

--- language/test/mirbench/src/index.rs
+++
@@ -1,6 +0,0 @@
-#[allow(unused_imports)]
-pub use super::program::{
-    BenchOptions, BenchOutputFormat, BenchProfile, BenchProfileKind, BenchTags, Program,
-    ProgramRunner, ResumeValueFn, ScaleAxis, print_stats, quick_bench, quick_bench_with_options,
-    quick_check, scale_axis, scale_axis_range, validate_all,
-};
