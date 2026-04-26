6ff4b86f4659e976592632e99ea01a3a1d8ade93
Florian Cäsar <florian.caesar@pm.me>
2026-04-26 12:03:07 +0200

    chore(language/linter): adapt to new IR shapes

--- language/linter/src/rules/complexity/no_complex_type.rs
+++ language/linter/src/rules/complexity/no_complex_type.rs
@@ -124,6 +124,7 @@ fn type_expression_complexity_inner(
             | TypeExpression::Index { .. }
             | TypeExpression::TemplateLiteral { .. }
             | TypeExpression::Tuple { .. }
+            | TypeExpression::ArrayTuple { .. }
             | TypeExpression::Object { .. }
             | TypeExpression::Array { .. }
     );
@@ -354,7 +355,7 @@ fn type_expression_complexity_inner(
                 ));
             }
         }
-        TypeExpression::Tuple { elements } => {
+        TypeExpression::Tuple { elements } | TypeExpression::ArrayTuple { elements } => {
             for element_id in elements {
                 let element = tree.get(*element_id);
                 match element {
