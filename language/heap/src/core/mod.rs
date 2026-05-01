444747534ce2e53ac66475fe499c96c15adb375b
Florian Cäsar <florian.caesar@pm.me>
2026-05-01 13:05:22 +0200

    refactor(language/heap): replace edge visitors with reference scans

--- language/heap/src/core/mod.rs
+++ language/heap/src/core/mod.rs
@@ -1,21 +1,21 @@
-mod edge;
 mod error;
 mod gc;
 mod layout;
 mod mark;
 mod payload;
+mod reference;
 mod root;
 mod table;
 mod usage;
 
-pub(crate) use edge::*;
-pub use edge::*;
 pub use error::*;
 pub use gc::*;
 pub(crate) use layout::*;
 pub use layout::*;
 pub(crate) use mark::*;
 pub use payload::*;
+pub(crate) use reference::*;
+pub use reference::*;
 pub use root::*;
 pub(crate) use table::*;
 pub use usage::*;
