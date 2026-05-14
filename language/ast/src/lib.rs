961ab6ca22e22a92aeaca786992122c07d7b2bcd
Florian Cäsar <florian.caesar@pm.me>
2026-05-14 17:07:00 +0200

    refactor(language/ast): remove AST

--- language/ast/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![feature(default_field_values)]
-#![feature(if_let_guard)]
-
-mod token;
-mod tree;
-
-pub use token::*;
-pub use tree::*;
--- language/ast/src/token/mod.rs
+++
@@ -1,9 +0,0 @@
-mod identifier;
-mod print;
-mod span;
-mod token;
-
-pub use identifier::*;
-pub use print::*;
-pub use span::*;
-pub use token::*;
