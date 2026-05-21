d9de0ae27af82629f042f2e91df725083ff83847
Florian Cäsar <florian.caesar@pm.me>
2026-05-22 00:24:50 +0200

    refactor(language/mir): require explicit closure environments

--- language/mir/src/build/tests.rs
+++ language/mir/src/build/tests.rs
@@ -608,7 +608,8 @@ fn test_type_construction() {
     let tuple_type = module.type_tuple(vec![i32_type, i64_type], Copy::Yes);
     let signature = module.type_function_signature(vec![i32_type], i32_type);
     let function_pointer_type = module.type_function_pointer(signature);
-    let callable_type = module.type_closure(signature);
+    let environment_type = module.tree_mut().ensure_callable_environment_type();
+    let callable_type = module.type_closure(signature, environment_type);
 
     // verify types
     let (tree, _strings) = module.finish();
