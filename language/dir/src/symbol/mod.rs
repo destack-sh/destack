8b230b6a7302f71140997de8f96c8777ffe160df
Florian Cäsar <florian.caesar@pm.me>
2026-05-21 15:25:09 +0200

    feat(language/dir): model indirect global entries

--- language/dir/src/symbol/mod.rs
+++ language/dir/src/symbol/mod.rs
@@ -1,5 +1,6 @@
 mod dependency;
 mod export;
+mod global;
 mod key;
 mod language;
 mod scope;
@@ -7,6 +8,7 @@ mod symbol;
 
 pub use dependency::*;
 pub use export::*;
+pub use global::*;
 pub use key::*;
 pub use language::*;
 pub use scope::*;
