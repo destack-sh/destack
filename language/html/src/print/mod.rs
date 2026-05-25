cc3c37f6f1a8bdadfde6bcce49914e19865d6b54
Florian Cäsar <florian.caesar@pm.me>
2026-05-25 12:37:32 +0200

    refactor(language/html): remove HTML IR

--- language/html/src/print/mod.rs
+++
@@ -1,9 +0,0 @@
-mod content;
-mod document;
-mod printer;
-
-pub use document::{
-    print_document, print_document_with_options, print_fragment, print_fragment_with_options,
-};
-pub use printer::PrintOptions;
-pub(crate) use printer::Printer;
