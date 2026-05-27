18dc166235da7ad3dca8adaba6871efeac80c5d6
Florian Cäsar <florian.caesar@pm.me>
2026-05-27 17:45:16 +0200

    chore(platform/site): edit prose

--- platform/site/src/home/hero.tsx
+++ platform/site/src/home/hero.tsx
@@ -2,29 +2,36 @@ import { Install } from "./install";
 
 export function Hero() {
     return (
-        <div class="grid min-w-0 grid-cols-[minmax(0,1fr)] gap-4 px-2 md:px-4 py-2 lg:grid-cols-[minmax(0,1fr)_29rem] lg:items-center">
-            <section class="min-w-0">
-                {/* title */}
-                <h1 class="max-w-4xl text-3xl leading-none font-extrabold md:text-3xl">
-                    engineer great software with confidence
-                </h1>
+        <div class="flex gap-y-1 md:gap-y-3 flex-col">
+            {/* title */}
+            <h1 class="max-w-4xl w-full text-3xl px-2 md:px-4 leading-none font-extrabold md:text-3xl">
+                build magnificient software that lasts
+            </h1>
 
-                {/* tagline */}
-                <p class="mt-4 max-w-3xl text-sm leading-6 font-bold text-neutral-700">
-                    <Underline>destack</Underline> is a fully integrated stack for building
-                    correct, optimal, integrated software based on a{" "}
-                    <Underline>unified, native TypeScript++ toolchain</Underline>, VM, compiler, linter, and runtime
-                </p>
+            {/* main */}
+            <div class="grid min-w-0 grid-cols-[minmax(0,1fr)] gap-4 px-2 md:px-4 lg:grid-cols-[minmax(0,1fr)_29rem] lg:items-center">
+                <section class="flex flex-col gap-y-1 md:gap-y-3">
+                    {/* tagline */}
+                    <p class="max-w-3xl text-sm leading-6 font-bold text-neutral-700">
+                        <Underline>destack</Underline> is a fully integrated stack for building
+                        correct, optimal, integrated software based on a{" "}
+                        <Underline>unified, native TypeScript++ toolchain</Underline>, VM, compiler, linter, and runtime
+                    </p>
 
-                {/* "features" */}
-                <ul class="mt-3 flex min-w-0 flex-wrap gap-x-5 gap-y-2 text-sm font-extrabold lowercase">
-                    <Claim>build precision software</Claim>
-                    <Claim>ship with care and pride</Claim>
-                    <Claim>sleep well every night</Claim>
-                </ul>
-            </section>
+                </section>
 
-            <Install />
+                {/* install */}
+                <Install />
+            </div>
+
+            {/* "features" */}
+            <ul class="px-2 md:px-4 flex min-w-0 flex-wrap gap-x-5 gap-y-2 text-sm font-extrabold lowercase">
+                <Claim>engineer precise systems</Claim>
+                <Claim>craft software with care</Claim>
+                <Claim>ship software confidently</Claim>
+                <Claim>sleep well every night</Claim>
+                <Claim>leverage proven tech</Claim>
+            </ul>
         </div>
     );
 }
