288566ba81090e5ac63dd65a8f367de47f27a970
Florian Cäsar <florian.caesar@pm.me>
2026-05-30 00:21:40 +0200

    feat(language/dir): add Expression.NewMaybe

--- language/dir/src/tree/walk.rs
+++ language/dir/src/tree/walk.rs
@@ -1227,7 +1227,7 @@ pub fn walk_expression<V: NodeVisitor + ?Sized>(
             }
         }
 
-        Expression::New { ty, arguments } => {
+        Expression::New { ty, arguments } | Expression::NewMaybe { ty, arguments } => {
             let ty_expression = tree.get(*ty);
             visitor.visit_type_expression(tree, *ty, ty_expression);
             for argument_id in arguments {
