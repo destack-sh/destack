daba7ddd045d45a486755648423539c8ce3e5904
Florian Cäsar <florian.caesar@pm.me>
2026-05-30 20:49:38 +0200

    refactor(language/runtime): remove stale worker handle machinery

--- language/runtime/src/runtime/heap/mod.rs
+++ language/runtime/src/runtime/heap/mod.rs
@@ -1,5 +1,4 @@
 mod collector;
-mod handle;
 mod options;
 mod root;
 mod runtime;
@@ -7,7 +6,6 @@ mod worker;
 
 pub use collector::*;
 pub use destack_heap::{GcState, GcStats};
-pub use handle::*;
 pub use options::*;
 pub(crate) use root::*;
 pub(crate) use runtime::*;
