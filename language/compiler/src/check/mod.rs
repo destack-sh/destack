637e3abe81b608f106b9f4a40c33b6091c5eac18
Florian Cäsar <florian.caesar@pm.me>
2026-06-11 16:03:33 +0200

    chore(languaeg/compiler/check): add InvalidStaticOperation

--- language/compiler/src/check/mod.rs
+++ language/compiler/src/check/mod.rs
@@ -1,8 +1,6 @@
 mod build;
 mod commit;
-mod constraint;
 mod decorator;
-mod dispatch;
 mod dump;
 mod error;
 mod flow;
@@ -10,29 +8,25 @@ mod import;
 mod language;
 mod obligation;
 mod provide;
-mod resolution;
-mod resolve;
+mod select;
 mod solve;
 mod state;
-mod term;
+mod r#type;
 mod walk;
 mod warning;
 
 pub use error::*;
 pub use warning::*;
 
-pub(in crate::check) use constraint::*;
 pub(in crate::check) use decorator::*;
-pub(in crate::check) use dispatch::*;
 pub(in crate::check) use dump::*;
 pub(in crate::check) use flow::*;
 pub(in crate::check) use language::*;
 pub(in crate::check) use obligation::*;
-pub(in crate::check) use resolution::*;
-pub(in crate::check) use resolve::*;
+pub(in crate::check) use select::*;
 pub(in crate::check) use solve::*;
 pub(in crate::check) use state::*;
-pub(in crate::check) use term::*;
+pub(in crate::check) use r#type::*;
 pub(in crate::check) use walk::*;
 
 #[cfg(test)]
