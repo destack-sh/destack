49559728ba758faa2ac0ec69629d6fec4028ae24
Florian Cäsar <florian.caesar@pm.me>
2026-06-11 16:18:16 +0200

    refactor(language/compiler/check): unify DIR and check tables

--- language/compiler/src/check/constraint/mod.rs
+++
@@ -1,9 +0,0 @@
-mod constraint;
-mod pattern;
-mod relation;
-mod r#static;
-
-pub(in crate::check) use constraint::*;
-pub(in crate::check) use pattern::*;
-pub(in crate::check) use relation::*;
-pub(in crate::check) use r#static::*;
