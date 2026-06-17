97abd5215637bb396247f67054adbc50c30f281c
Florian Cäsar <florian.caesar@pm.me>
2026-06-17 22:22:24 +0200

    test(language/compiler/check): move check library tests into own module

--- language/compiler/src/check/tests/library/builtin.rs
+++ language/compiler/src/check/tests/library/builtin.rs
@@ -7,7 +7,7 @@ const DEFAULT_TARGET: &str = "default";
 
 /// Check every builtin library module.
 #[test]
-fn test_check_library() -> Result<(), String> {
+fn test_check_builtin_library_modules() -> Result<(), String> {
     let session = TestSession::builder().build();
     let repository = session.repository();
     let package = repository.builtin_package();
--- language/compiler/src/check/tests/library/mod.rs
+++ language/compiler/src/check/tests/library/mod.rs
@@ -1,1 +1,1 @@
-mod check;
+mod builtin;
