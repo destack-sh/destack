d91f8e558f87aad7b970918ab0beb8eed6e299d5
Florian Cäsar <florian.caesar@pm.me>
2026-06-20 12:34:12 +0200

    feat(language/program): model durable native code payloads

--- language/program/src/native/mod.rs
+++ language/program/src/native/mod.rs
@@ -1,5 +1,17 @@
-mod executable;
-mod relocation;
+mod abi;
+mod code;
+mod entry;
+mod image;
+mod import;
+mod library;
+mod map;
+mod object;
 
-pub use executable::*;
-pub use relocation::*;
+pub use abi::*;
+pub use code::*;
+pub use entry::*;
+pub use image::*;
+pub use import::*;
+pub use library::*;
+pub use map::*;
+pub use object::*;
