048aac7430f28623a3ac5bfcb1fe82f11d9a40a0
Florian Cäsar <florian.caesar@pm.me>
2026-06-25 21:15:36 +0200

    feat(language/repository): generalise artifact trace timeline rendering

--- language/repository/src/provider/mod.rs
+++ language/repository/src/provider/mod.rs
@@ -2,10 +2,12 @@ mod context;
 mod error;
 mod recorder;
 mod resolve;
+mod timeline;
 mod trace;
 
 pub use context::*;
 pub use error::*;
 pub use recorder::*;
 pub use resolve::*;
+pub use timeline::*;
 pub use trace::*;
