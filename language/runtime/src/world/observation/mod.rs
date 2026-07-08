d8353635de150734784917db7997f8475d11243c
Florian Cäsar <florian.caesar@pm.me>
2026-07-08 15:58:36 +0200

    feat(language/runtime): close observation model

--- language/runtime/src/world/observation/mod.rs
+++ language/runtime/src/world/observation/mod.rs
@@ -1,19 +1,15 @@
-mod category;
 mod chunk;
 mod entry;
 mod log;
 mod observation;
-mod options;
 mod scope;
 mod sequence;
 mod store;
 
-pub use category::*;
 pub(crate) use chunk::*;
 pub use entry::*;
 pub use log::*;
 pub use observation::*;
-pub use options::*;
 pub use scope::*;
 pub use sequence::*;
 pub(crate) use store::*;
