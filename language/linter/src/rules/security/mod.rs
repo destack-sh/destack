12eb1beb660f842a226f8726dd2143853ec7e6ef
Florian Cäsar <florian.caesar@pm.me>
2026-08-03 18:03:27 +0200

    refactor(language/linter): remove compiler-owned and non-universal rules

--- language/linter/src/rules/security/mod.rs
+++ language/linter/src/rules/security/mod.rs
@@ -1,7 +1,6 @@
 mod hardcoded_secret;
 mod no_invisible_character;
 mod no_permissive_file_permission;
-mod no_space_in_command_argument;
 mod no_super_linear_regex;
 mod no_tainted_sink;
 mod undocumented_unsafe;
@@ -9,7 +8,6 @@ mod undocumented_unsafe;
 pub use hardcoded_secret::*;
 pub use no_invisible_character::*;
 pub use no_permissive_file_permission::*;
-pub use no_space_in_command_argument::*;
 pub use no_super_linear_regex::*;
 pub use no_tainted_sink::*;
 pub use undocumented_unsafe::*;
