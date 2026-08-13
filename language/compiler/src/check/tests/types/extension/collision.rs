36709a864c8a0ae974fd29c88ed9b34d7e255f70
Florian Cäsar <florian.caesar@pm.me>
2026-08-13 15:20:36 +0200

    test(language/compiler/check): pin the unnamed exported nonlocal extension rule

--- language/compiler/src/check/tests/types/extension/collision.rs
+++ language/compiler/src/check/tests/types/extension/collision.rs
@@ -125,3 +125,55 @@ const size = widget.size;
 "#,
     );
 }
+
+#[test]
+fn test_reject_an_unnamed_exported_extension_of_a_nonlocal_type() {
+    let session = TestSession::builder()
+        .module(
+            "widget.ds",
+            r#"
+export struct Widget {}
+"#,
+        )
+        .module(
+            "main.ds",
+            r#"
+import { Widget } from "./widget.ds";
+
+export extension of Widget {
+    get size(): usize {
+        return 1;
+    }
+}
+"#,
+        )
+        .build();
+
+    session.assert_dir_checked_and_diagnostics(
+        "main.ds",
+        DirRows::none(),
+        r#"
+=== annotated ===
+import { Widget } from "./widget.ds";
+
+export extension of Widget {
+    get size(): usize {
+        return 1;
+    }
+}
+
+=== checked ===
+import { Widget } from "./widget.ds";
+
+export extension of Widget {
+    get size(): usize {
+        return 1;
+    }
+}
+"#,
+        r#"
+/// @diagnostic.error id=unnamed-exported-nonlocal-extension message="exported extension on nonlocal type 'Widget' must have a name"
+/// @diagnostic.label line=4 column=21 span="Widget" line_source="export extension of Widget {"
+"#,
+    );
+}
