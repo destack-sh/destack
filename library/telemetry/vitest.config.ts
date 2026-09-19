61b6473a0835df7116b41b199fdc993a9372e14a
Florian Cäsar <florian.caesar@pm.me>
2026-09-19 17:20:43 +0200

    test(library/telemetry): remove unused test configuration

--- library/telemetry/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@destack/test/config";
-
-export default defineConfig({
-    test: {
-        include: ["src/**/*.test.ts"],
-        testTimeout: 1000,
-        hookTimeout: 1000,
-    },
-});
