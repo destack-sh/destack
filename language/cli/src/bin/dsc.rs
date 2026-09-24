6372e0d7e10594e8a830c062b675501859b44d0e
Florian Cäsar <florian.caesar@pm.me>
2026-09-24 13:00:20 +0200

    chore(language/cli): format dsc

--- language/cli/src/bin/dsc.rs
+++ language/cli/src/bin/dsc.rs
@@ -1,4 +1,5 @@
 fn main() {
-    let exit_code = destack_language_cli::app::run(destack_language_cli::app::DefaultCommand::Build);
+    let exit_code =
+        destack_language_cli::app::run(destack_language_cli::app::DefaultCommand::Build);
     std::process::exit(exit_code);
 }
